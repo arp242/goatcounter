78e56493edcfbaaf37c1f22b6278489ac1cc449a
Martin Tournoij <martin@arp242.net>
2019-11-11 15:24:05 +0800

    Admin: link to public sites

--- admin.go
+++ admin.go
@@ -17,6 +17,7 @@ type AdminStat struct {
 	Name      string    `db:"name"`
 	User      string    `db:"user"`
 	Email     string    `db:"email"`
+	Public    bool      `db:"public"`
 	CreatedAt time.Time `db:"created_at"`
 	Count     int       `db:"count"`
 }
@@ -32,11 +33,12 @@ func (a *AdminStats) List(ctx context.Context) error {
 			sites.created_at,
 			users.name as user,
 			users.email,
+			settings::json->>'public' as public,
 			count(*) - 1 as count
 		from sites
 		left join hits on hits.site=sites.id
 		join users on users.site=sites.id
-		group by sites.code, sites.name, sites.created_at, users.name, users.email
+		group by sites.code, sites.name, sites.created_at, users.name, users.email, public
 		order by count desc`)
 	return errors.Wrap(err, "AdminStats.List")
 }
