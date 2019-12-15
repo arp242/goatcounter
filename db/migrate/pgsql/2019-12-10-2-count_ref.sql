3985927102517c3ee20a2bd0243099a799df4f88
Martin Tournoij <martin@arp242.net>
2019-12-15 16:11:03 +0800

    Remove some old columns

--- db/migrate/pgsql/2019-12-10-2-count_ref.sql
+++ db/migrate/pgsql/2019-12-10-2-count_ref.sql
@@ -3,4 +3,3 @@ begin;
 		add column count_ref varchar not null default '';
 	insert into version values ('2019-12-10-2-count_ref');
 commit;
-
--- pack/pack.go
+++ pack/pack.go
@@ -372,7 +372,12 @@ commit;
 		add column count_ref varchar not null default '';
 	insert into version values ('2019-12-10-2-count_ref');
 commit;
-
+`),
+	"db/migrate/pgsql/2019-12-15-2-old.sql": []byte(`begin;
+	alter table hit_stats drop column updated_at;
+	alter table hit_stats drop column created_at;
+	insert into version values ('2019-12-15-2-old');
+commit;
 `),
 }
 
@@ -756,6 +761,26 @@ commit;
 	insert into version values ('2019-12-10-2-count_ref');
 commit;
 
+`),
+	"db/migrate/sqlite/2019-12-15-2-old.sql": []byte(`begin;
+	create temporary table hit_stats2 (
+		site           integer        not null                 check(site > 0),
+
+		day            date           not null                 check(day = strftime('%Y-%m-%d', day)),
+		path           varchar        not null,
+		stats          varchar        not null,
+
+		foreign key (site) references sites(id) on delete restrict on update restrict
+	);
+
+	insert into hit_stats2 select site, day, path, stats from hit_stats;
+
+	drop table hit_stats;
+	alter table hit_stats2 rename to hit_stats;
+	create index "hit_stats#site#day" on hit_stats(site, day);
+
+	insert into version values ('2019-12-15-2-old');
+commit;
 `),
 }
 
