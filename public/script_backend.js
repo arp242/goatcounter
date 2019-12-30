9525be97bffe896b0529c0f1e041d63847ea007f
Martin Tournoij <martin@arp242.net>
2019-12-30 21:24:02 +0800

    Update copyright notice: add http://

--- public/script_backend.js
+++ public/script_backend.js
@@ -1,6 +1,6 @@
 // Copyright © 2019 Martin Tournoij <martin@arp242.net>
 // This file is part of GoatCounter and published under the terms of the EUPL
-// v1.2, which can be found in the LICENSE file or at eupl12.zgo.at
+// v1.2, which can be found in the LICENSE file or at http://eupl12.zgo.at
 
 (function() {
 	'use strict';
