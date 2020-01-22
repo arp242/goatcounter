arp242/goatcounter

commit 2884ab364d90752c439b51d52a3f0edb019a93d2

Allow configuring the thousands separator (#132)
Fixes #124

handlers/backend.go
@@ -5,6 +5,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/csv"
 	"fmt"
 	"html/template"
@@ -448,7 +449,10 @@ func (h backend) refs(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	tpl, err := zhttp.ExecuteTpl("_backend_refs.gohtml", refs)
+	tpl, err := zhttp.ExecuteTpl("_backend_refs.gohtml", map[string]interface{}{
+		"Refs": refs,
+		"Site": goatcounter.MustGetSite(r.Context()),
+	})
 	if err != nil {
 		return err
 	}
@@ -476,9 +480,9 @@ func (h backend) browsers(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	f := zhttp.FuncMap["hbar_chart"].(func(goatcounter.Stats, int, int, float32, bool) template.HTML)
+	f := zhttp.FuncMap["hbar_chart"].(func(context.Context, goatcounter.Stats, int, int, float32, bool) template.HTML)
 	t, _ := strconv.ParseInt(r.URL.Query().Get("total"), 10, 64)
-	tpl := f(browsers, total, int(t), .5, true)
+	tpl := f(r.Context(), browsers, total, int(t), .5, true)
 
 	return zhttp.JSON(w, map[string]interface{}{
 		"html": string(tpl),
@@ -502,9 +506,9 @@ func (h backend) sizes(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	f := zhttp.FuncMap["hbar_chart"].(func(goatcounter.Stats, int, int, float32, bool) template.HTML)
+	f := zhttp.FuncMap["hbar_chart"].(func(context.Context, goatcounter.Stats, int, int, float32, bool) template.HTML)
 	t, _ := strconv.ParseInt(r.URL.Query().Get("total"), 10, 64)
-	tpl := f(sizeStat, total, int(t), .5, true)
+	tpl := f(r.Context(), sizeStat, total, int(t), .5, true)
 
 	return zhttp.JSON(w, map[string]interface{}{
 		"html": string(tpl),
@@ -528,8 +532,8 @@ func (h backend) locations(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	f := zhttp.FuncMap["hbar_chart"].(func(goatcounter.Stats, int, int, float32, bool) template.HTML)
-	tpl := f(locStat, total, total, 0, false)
+	f := zhttp.FuncMap["hbar_chart"].(func(context.Context, goatcounter.Stats, int, int, float32, bool) template.HTML)
+	tpl := f(r.Context(), locStat, total, total, 0, false)
 	return zhttp.JSON(w, map[string]interface{}{
 		"html": string(tpl),
 	})

.gitignore
@@ -3,3 +3,4 @@
 /*.upx
 /db/*.sqlite3*
 /dist
+/b.sql

db/migrate/pgsql/2020-01-23-1-nformat.sql
@@ -1,0 +1,10 @@
+begin;
+	insert into updates (subject, created_at, show_at, body) values (
+		'New setting: thousands separator', now(), now(),
+		'<p>You can now choose which thousands separators is used to format
+		numbers in your site’s settings. The default is still a thin space as
+		before, as that’s the most universal format.</p>');
+
+	update sites set settings=substr(settings, 0, length(settings)) || ', "number_format": 8239}';
+	insert into version values ('2020-01-23-1-nformat');
+commit;

db/migrate/sqlite/2020-01-23-1-nformat.sql
@@ -1,0 +1,4 @@
+begin;
+	update sites set settings=substr(settings, 0, length(settings)) || ', "number_format": 8239}';
+	insert into version values ('2020-01-23-1-nformat');
+commit;

go.mod
@@ -4,18 +4,18 @@ go 1.12
 
 require (
 	github.com/arp242/geoip2-golang v1.4.0
-	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.3.0
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible
 	github.com/monoculum/formam v0.0.0-20191229172733-952f0766a724
 	github.com/mssola/user_agent v0.5.0
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/teamwork/guru v1.0.0
 	github.com/teamwork/reload v1.3.0
-	zgo.at/utils v1.2.0
+	zgo.at/utils v1.3.1
 	zgo.at/zdb v0.0.0-20200114054604-cb5410b595af
-	zgo.at/zhttp v0.0.0-20200115164934-3af3cb27bf0b
+	zgo.at/zhttp v0.0.0-20200122175535-f8103001dd8e
 	zgo.at/zlog v1.0.8
 	zgo.at/zpack v1.0.0
 	zgo.at/zstripe v1.0.0

go.sum
@@ -6,6 +6,8 @@ github.com/fsnotify/fsnotify v1.4.7 h1:IXs+QLmnXW2CcXuY+8Mzv/fWEsPGWxqefPtCP5CnV
 github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMoQvtojpjFo=
 github.com/go-chi/chi v4.0.2+incompatible h1:maB6vn6FqCxrpz4FqWdh4+lwpyZIQS7YEAUcHlgXVRs=
 github.com/go-chi/chi v4.0.2+incompatible/go.mod h1:eB3wogJHnLi3x/kFX2A+IbTBlXxmMeXJVKy9tTv1XzQ=
+github.com/go-chi/chi v4.0.3+incompatible h1:gakN3pDJnzZN5jqFV2TEdF66rTfKeITyR8qu6ekICEY=
+github.com/go-chi/chi v4.0.3+incompatible/go.mod h1:eB3wogJHnLi3x/kFX2A+IbTBlXxmMeXJVKy9tTv1XzQ=
 github.com/go-sql-driver/mysql v1.4.0 h1:7LxgVwFb2hIQtMm87NdgAVfXjnt4OePseqT1tKx+opk=
 github.com/go-sql-driver/mysql v1.4.0/go.mod h1:zAC/RDZ24gD3HViQzih4MyKcchzm+sOG5ZlKdlhCg5w=
 github.com/golang/protobuf v1.3.1 h1:YF8+flBXS5eO826T4nzqPrxfhQThhXl0YzfuUPu4SBg=
@@ -28,6 +30,8 @@ github.com/mssola/user_agent v0.5.0 h1:gRF7/x8cKt8qzAosYGsBNyirta+F8fvYDlJrgXws9
 github.com/mssola/user_agent v0.5.0/go.mod h1:UFiKPVaShrJGW93n4uo8dpPdg1BSVpw2P9bneo0Mtp8=
 github.com/pkg/errors v0.8.1 h1:iURUrRGxPUNPdy5/HRSm+Yj6okJ6UtLINN0Q9M4+h3I=
 github.com/pkg/errors v0.8.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
+github.com/pkg/errors v0.9.1 h1:FEBLx1zS214owpjy7qsBeixbURkuhQAwrK5UwLGTwt4=
+github.com/pkg/errors v0.9.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
 github.com/teamwork/guru v1.0.0 h1:iOnJwnmfZnwp6ny7+G7/h8OtLjuB9XxmSh5ikwaljzQ=
 github.com/teamwork/guru v1.0.0/go.mod h1:1DW23kFX0aXn1kRSuQAY+Byk74h1kZHvhJL72qBUrfU=
 github.com/teamwork/reload v1.3.0 h1:ElBaBhtRwVRxsPRGK1wYugr2knciOXFMZT5ltPpEwAI=
@@ -48,14 +52,12 @@ google.golang.org/appengine v1.6.5 h1:tycE03LOZYQNhDpS27tcQdAzLCVMaj7QT2SXxebnpC
 google.golang.org/appengine v1.6.5/go.mod h1:8WjMMxjGQR8xUklV/ARdw2HLXBOI7O7uCIDZVag1xfc=
 zgo.at/utils v1.2.0 h1:Ohv+XfqyoiSdl9lCbyiDZ5E4YgEXWxxE4Y/holHDiYo=
 zgo.at/utils v1.2.0/go.mod h1:7acz1AfMZAOOC1wTi6QbYFie+eA0LRSHE38TwLZsFcA=
+zgo.at/utils v1.3.1 h1:e5/HyvCEuASSrpipLY0QhWMjDIYr+zU24wRJzejX+es=
+zgo.at/utils v1.3.1/go.mod h1:7acz1AfMZAOOC1wTi6QbYFie+eA0LRSHE38TwLZsFcA=
 zgo.at/zdb v0.0.0-20200114054604-cb5410b595af h1:0sqdYwYaBk8qDdNEW/gmmZ0Otxih4wYMbFYnPnO77ow=
 zgo.at/zdb v0.0.0-20200114054604-cb5410b595af/go.mod h1:GT5Nyrxb9H6JszZtd7o0m5CPEd3C7DFqAeJt98nyNQI=
-zgo.at/zhttp v0.0.0-20200113161827-2cb6ed03333e h1:sZ1oj9sa+w6mQlhoxMMWWKqMAPEZURHIR0cY1St3Mgc=
-zgo.at/zhttp v0.0.0-20200113161827-2cb6ed03333e/go.mod h1:7PO/E4nqxbstyxX95cqKJOpZAVKNKoohk5utWZIdKz0=
-zgo.at/zhttp v0.0.0-20200115163454-886ef9c5533a h1:0atOlSU71LJJ+7B9dXHutLxyJCqiXQduOzwgv46vJP4=
-zgo.at/zhttp v0.0.0-20200115163454-886ef9c5533a/go.mod h1:7PO/E4nqxbstyxX95cqKJOpZAVKNKoohk5utWZIdKz0=
-zgo.at/zhttp v0.0.0-20200115164934-3af3cb27bf0b h1:xTXb69qkPG1I2glFT/S1OSjpF+1TPW7xMlJioqJ3Tyw=
-zgo.at/zhttp v0.0.0-20200115164934-3af3cb27bf0b/go.mod h1:7PO/E4nqxbstyxX95cqKJOpZAVKNKoohk5utWZIdKz0=
+zgo.at/zhttp v0.0.0-20200122175535-f8103001dd8e h1:LI0CLO0xNyDLc0Mktjoaq2UBZhx2A32lpx5GOOM7J4w=
+zgo.at/zhttp v0.0.0-20200122175535-f8103001dd8e/go.mod h1:7PO/E4nqxbstyxX95cqKJOpZAVKNKoohk5utWZIdKz0=
 zgo.at/zlog v1.0.7 h1:Ppjb9VOBheqENQ28TWsOddG2qIVO4nGb0b/TPh/5sXc=
 zgo.at/zlog v1.0.7/go.mod h1:0RldP/sQzWwOCPsJogZcxnJneFt56T1zfDtIN/LcYec=
 zgo.at/zlog v1.0.8 h1:uXgCFWdGkixSrGD83zjMFHSk3OD9V89UaN9KANO+yCM=

init.go
@@ -39,6 +39,10 @@ func init() {
 		return s.URL()
 	}
 
+	zhttp.FuncMap["nformat2"] = func(n int, s Site) string {
+		return zhttp.FuncMap["nformat"].(func(int, rune) string)(n, s.Settings.NumberFormat)
+	}
+
 	zhttp.FuncMap["validate"] = func(k string, v map[string][]string) template.HTML {
 		if v == nil {
 			return template.HTML("")
@@ -52,7 +56,7 @@ func init() {
 	}
 
 	// Implemented as function for performance.
-	zhttp.FuncMap["bar_chart"] = func(stats []Stat, max int) template.HTML {
+	zhttp.FuncMap["bar_chart"] = func(ctx context.Context, stats []Stat, max int) template.HTML {
 		var b strings.Builder
 		now := time.Now().UTC()
 		today := now.Format("2006-01-02")
@@ -74,14 +78,14 @@ func init() {
 					inner = fmt.Sprintf(`<div style="height: %.0f%%;"></div>`, h)
 				}
 				b.WriteString(fmt.Sprintf(`<div title="%s %[2]d:00 – %[2]d:59, %s views">%s</div>`,
-					stat.Day, shour, zhttp.Tnformat(s), inner))
+					stat.Day, shour, zhttp.Tnformat(s, MustGetSite(ctx).Settings.NumberFormat), inner))
 			}
 		}
 
 		return template.HTML(b.String())
 	}
 
-	zhttp.FuncMap["hbar_chart"] = func(stats Stats, total, parentTotal int, cutoff float32, link bool) template.HTML {
+	zhttp.FuncMap["hbar_chart"] = func(ctx context.Context, stats Stats, total, parentTotal int, cutoff float32, link bool) template.HTML {
 		tag := "p"
 		if link {
 			tag = "a"
@@ -105,7 +109,8 @@ func init() {
 			}
 
 			title := fmt.Sprintf("%s: %.1f%% – %s hits in total",
-				template.HTMLEscapeString(browser), perc, zhttp.Tnformat(s.Count))
+				template.HTMLEscapeString(browser), perc,
+				zhttp.Tnformat(s.Count, MustGetSite(ctx).Settings.NumberFormat))
 			b.WriteString(fmt.Sprintf(
 				`<%[4]s href="#_" title="%[1]s"><small>%[2]s</small> <span style="width: %[3]f%%">%.1[3]f%%</span></%[4]s>`,
 				title, template.HTMLEscapeString(browser), perc, tag))

pack/pack.go
@@ -482,6 +482,17 @@ commit;
 	insert into version values ('2020-01-18-1-sitename');
 commit;
 `),
+	"db/migrate/pgsql/2020-01-23-1-nformat.sql": []byte(`begin;
+	insert into updates (subject, created_at, show_at, body) values (
+		'New setting: thousands separator', now(), now(),
+		'<p>You can now choose which thousands separators is used to format
+		numbers in your site’s settings. The default is still a thin space as
+		before, as that’s the most universal format.</p>');
+
+	update sites set settings=substr(settings, 0, length(settings)) || ', "number_format": 8239}';
+	insert into version values ('2020-01-23-1-nformat');
+commit;
+`),
 }
 
 var MigrationsSQLite = map[string][]byte{
@@ -998,6 +1009,11 @@ commit;
 	"db/migrate/sqlite/2020-01-18-1-sitename.sql": []byte(`begin;
 	drop index "sites#name";
 	insert into version values ('2020-01-18-1-sitename');
+commit;
+`),
+	"db/migrate/sqlite/2020-01-23-1-nformat.sql": []byte(`begin;
+	update sites set settings=substr(settings, 0, length(settings)) || ', "number_format": 8239}';
+	insert into version values ('2020-01-23-1-nformat');
 commit;
 `),
 }
@@ -13491,7 +13507,7 @@ var Templates = map[string][]byte{
 `),
 	"tpl/_backend_pages.gohtml": []byte(`{{range $h := .Pages}}
 	<tr id="{{$h.Path}}"{{if eq $h.Path $.ShowRefs}}class="target"{{end}}>
-		<td>{{$h.Count | nformat}}</td>
+		<td>{{nformat2 $h.Count $.Site}}</td>
 		<td class="hide-mobile">
 			<a class="rlink" title="{{$h.Path}}" href="?showrefs={{$h.Path}}&period-start={{tformat $.PeriodStart ""}}&period-end={{tformat $.PeriodEnd ""}}#{{$h.Path}}">{{$h.Path}}</a><br>
 			<small class="page-title" title="{{$h.Title}}">{{if $h.Title}}{{$h.Title}}{{else}}<em>(no title)</em>{{end}}</small>
@@ -13505,12 +13521,12 @@ var Templates = map[string][]byte{
 				{{/* We don't have the site domain, so can't link to it.
 				<a class="top go" target="_blank" rel="noopener" href="https://{{$.Site.Name}}{{$h.Path}}">go</a>
 				*/}}
-				<span class="top max" title="Y-axis scale">{{nformat .Max}}</span>
+				<span class="top max" title="Y-axis scale">{{nformat2 .Max $.Site}}</span>
 				<span class="half"></span>
-				{{bar_chart .Stats .Max}}
+				{{bar_chart $.Context .Stats .Max}}
 			</div>
 			<div class="refs">{{if and $.Refs (eq $.ShowRefs $h.Path)}}
-				{{template "_backend_refs.gohtml" $.Refs}}
+				{{template "_backend_refs.gohtml" map "Refs" $.Refs "Site" $.Site}}
 				{{if $.MoreRefs}}<a href="#_", class="load-more-refs">load more</a>{{end}}
 			{{end}}</div>
 		</td>
@@ -13520,9 +13536,9 @@ var Templates = map[string][]byte{
 {{- end}}
 `),
 	"tpl/_backend_refs.gohtml": []byte(`<table class="count-list count-list-refs"><tbody>
-{{range $r := .}}
+{{range $r := .Refs}}
 	<tr>
-		<td>{{$r.Count | nformat}}</td>
+		<td>{{nformat2 $r.Count $.Site}}</td>
 		<td{{if or (eq (deref_s $r.RefScheme) "g") (eq $r.Path "")}} class="generated"{{end}}>
 			{{if $r.Path}}{{$r.Path}}
 			{{if ne (deref_s $r.RefScheme) "g"}}<sup><a class="go" href="http://{{$r.Path}}" target="_blank" rel="noopener">go</a></sup>{{end}}
@@ -13875,7 +13891,7 @@ parameters:</p>
 	<div class="pages-list">
 		<header class="h2 header-pages">
 			<h2>Pages</h2>
-			<sup>(total <span class="total-hits">{{nformat .TotalHits}}</span> hits, <span class="total-display">{{nformat .TotalHitsDisplay}}</span> displayed)</sup>
+			<sup>(total <span class="total-hits">{{nformat2 .TotalHits $.Site}}</span> hits, <span class="total-display">{{nformat2 .TotalHitsDisplay $.Site}}</span> displayed)</sup>
 			<input autocomplete="off" name="filter" value="{{.Filter}}" id="filter-paths" placeholder="Filter paths"
 				{{if .Filter}}class="value"{{end}}
 				title="Filter the list of paths; matched case-insensitive on path and title">
@@ -13897,7 +13913,7 @@ parameters:</p>
 		{{if eq .TotalBrowsers 0}}
 			<em>Nothing to display</em>
 		{{else}}
-			<div class="chart-hbar" data-detail="/browsers">{{hbar_chart .Browsers .TotalBrowsers 0 .5 true}}</div>
+			<div class="chart-hbar" data-detail="/browsers">{{hbar_chart .Context .Browsers .TotalBrowsers 0 .5 true}}</div>
 		{{end}}
 	</div>
 	<div>
@@ -13905,7 +13921,7 @@ parameters:</p>
 		{{if eq .TotalHits 0}}
 			<em>Nothing to display</em>
 		{{else}}
-			<div class="chart-hbar" data-detail="/sizes">{{hbar_chart .SizeStat .TotalSize 0 0.1 true}}</div>
+			<div class="chart-hbar" data-detail="/sizes">{{hbar_chart .Context .SizeStat .TotalSize 0 0.1 true}}</div>
 			<p><small>The screen sizes are an indication and influenced by DPI and zoom levels.
 				{{/*Approximately {{.TotalMobile}}% advertised the usage of a mobile browser.*/}}
 			</small></p>
@@ -13916,7 +13932,7 @@ parameters:</p>
 		{{if eq .TotalHits 0}}
 			<em>Nothing to display</em>
 		{{else}}
-			<div class="chart-hbar">{{hbar_chart .LocationStat .TotalLocation 0 3 false}}</div>
+			<div class="chart-hbar">{{hbar_chart .Context .LocationStat .TotalLocation 0 3 false}}</div>
 			{{if .ShowMoreLocations}}<a href="#" class="show-all">Show all</a>{{end}}
 		{{end}}
 	</div>
@@ -13944,7 +13960,7 @@ parameters:</p>
 	{{range $s := .Stats}}
 		<tr id="{{$s.ID}}">
 			<td><a href="/admin/{{$s.ID}}">{{$s.ID}}</a></td>
-			<td>{{nformat $s.Count}}</td>
+			<td>{{nformat2 $s.Count $.Site}}</td>
 			<td>
 				{{if $s.Public}}
 					<a href="https://{{$s.Code}}.{{$.Domain}}">{{$s.Code}}</a>
@@ -13989,9 +14005,9 @@ parameters:</p>
 <h2>Admin</h2>
 
 <table>
-	<tr><td>Total</td><td>{{.Stat.CountTotal | nformat}}</td></tr>
-	<tr><td>Last month</td><td>{{.Stat.CountLastMonth | nformat}}</td></tr>
-	<tr><td>Previous month</td><td>{{.Stat.CountPrevMonth | nformat}}</td></tr>
+	<tr><td>Total</td><td>{{nformat2 .Stat.CountTotal $.Site}}</td></tr>
+	<tr><td>Last month</td><td>{{nformat2 .Stat.CountLastMonth $.Site}}</td></tr>
+	<tr><td>Previous month</td><td>{{nformat2 .Stat.CountPrevMonth $.Site}}</td></tr>
 	</tr>
 </table>
 
@@ -14010,7 +14026,7 @@ parameters:</p>
 		<thead><tr><th style="width: 10em"># of hits</th><th style="text-align: left">Path</th></tr></thead></thead>
 		<tbody>
 			{{range $s := .List}}
-				<tr><td>{{nformat $s.Count}}</td><td>{{$s.Path}}</td></tr>
+				<tr><td>{{nformat2 $s.Count $.Site}}</td><td>{{$s.Path}}</td></tr>
 			{{end}}
 		</tbody>
 	</table>
@@ -14090,8 +14106,8 @@ parameters:</p>
 				<label for="user.email">Your email</label>
 				<input type="text" name="user.email" id="user.email" value="{{.User.Email}}">
 				{{validate "user.email" .Validate}}
-
 				<span>You will need access to the inbox to sign in.</span>
+
 				<label for="date_format">Date format</label>
 				<select name="settings.date_format" id="date_format">
 					<option {{option_value .Site.Settings.DateFormat "2006-01-02"}}>year-month-day (2006-01-02)</option>
@@ -14103,6 +14119,17 @@ parameters:</p>
 
 				<label>{{checkbox .Site.Settings.TwentyFourHours "settings.twenty_four_hours"}}
 					24-hour clock</label>
+
+				<label for="number_format">Thousands separator</label>
+				<select name="settings.number_format" id="number_format">
+					<option {{option_value (string .Site.Settings.NumberFormat) "8239"}}>Thin space (42 123)</option>
+					<option {{option_value (string .Site.Settings.NumberFormat) "160"}}>Space (42 123)</option>
+					<option {{option_value (string .Site.Settings.NumberFormat) "44"}}>Comma (42,123)</option>
+					<option {{option_value (string .Site.Settings.NumberFormat) "46"}}>Dot (42.1234)</option>
+					<option {{option_value (string .Site.Settings.NumberFormat) "39"}}>Apostrophe (42'123)</option>
+					<option {{option_value (string .Site.Settings.NumberFormat) "1"}}>None (42123)</option>
+				</select>
+				{{validate "settings.number_format" .Validate}}
 
 				<label for="limits_page">Page size</label>
 				<input type="text" name="settings.limits.page" id="limits_page" value="{{.Site.Settings.Limits.Page}}">

commit fce00961178f1782b1850c979916c76050d20cbc

Fix "load more" for pages
Was broken after 2884ab3 >_<

handlers/backend.go
@@ -558,14 +558,16 @@ func (h backend) pages(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	tpl, err := zhttp.ExecuteTpl("_backend_pages.gohtml", struct {
+		Context     context.Context
 		Pages       goatcounter.HitStats
+		Site        *goatcounter.Site
 		PeriodStart time.Time
 		PeriodEnd   time.Time
 
 		// Dummy values so template won't error out.
 		Refs     bool
 		ShowRefs string
-	}{pages, start, end, false, ""})
+	}{r.Context(), pages, goatcounter.MustGetSite(r.Context()), start, end, false, ""})
 	if err != nil {
 		return err
 	}