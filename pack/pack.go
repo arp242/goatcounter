d74216988fc5c4d0a5bf8f2102b635cf03a63080
Martin Tournoij <martin@arp242.net>
2020-01-23 13:23:30 +0800

    update pack.go

--- pack/pack.go
+++ pack/pack.go
@@ -496,7 +496,7 @@ commit;
 	"db/migrate/pgsql/2020-01-23-2-retention.sql": []byte(`begin;
 	insert into updates (subject, created_at, show_at, body) values (
 		'New setting: data retention', now(), now(),
-		'<p>You can now limit the amount of time GoatCounter keeps data in you site settings.</p>');
+		'<p>You can now limit the amount of time GoatCounter keeps data in your site settings.</p>');
 
 	insert into version values ('2020-01-23-2-retention');
 commit;
