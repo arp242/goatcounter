9baac7af27a048a6d795015993700ceb92d1b709
Martin Tournoij <martin@arp242.net>
2020-03-15 17:40:23 +0800

    Also clear usage table on site deletion

--- cron/cron.go
+++ cron/cron.go
@@ -256,7 +256,7 @@ func vacuumDeleted(ctx context.Context) error {
 		zlog.Module("vacuum").Printf("vacuum site %s/%d", s.Code, s.ID)
 
 		err := zdb.TX(ctx, func(ctx context.Context, db zdb.DB) error {
-			for _, t := range []string{"browser_stats", "hit_stats", "hits", "location_stats", "ref_stats", "users"} {
+			for _, t := range []string{"usage", "browser_stats", "hit_stats", "hits", "location_stats", "ref_stats", "users"} {
 				_, err := db.ExecContext(ctx, fmt.Sprintf(`delete from %s where site=%d`, t, s.ID))
 				if err != nil {
 					return fmt.Errorf("%s: %w", t, err)
