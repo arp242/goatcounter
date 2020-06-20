62933ea460f01793fd7f80e3b2d4412cf0a1a4b3
Martin Tournoij <martin@arp242.net>
2020-06-21 02:40:33 +0800

    Release public/count.js under ISC license
    Fixes #309

--- public/count.js
+++ public/count.js
@@ -1,4 +1,6 @@
 // GoatCounter: https://www.goatcounter.com
+// This file (and *only* this file) is released under the ISC license:
+// https://opensource.org/licenses/ISC
 (function() {
 	'use strict';
 
