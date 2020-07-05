df901c12d3a1de3b198d73e4999f49ad2cd07300
Martin Tournoij <martin@arp242.net>
2020-07-05 22:45:16 +0800

    Update license headers

--- acme/acme.go
+++ acme/acme.go
@@ -1,6 +1,6 @@
-// Copyright © 2019 Martin Tournoij <martin@arp242.net>
-// This file is part of GoatCounter and published under the terms of the EUPL
-// v1.2, which can be found in the LICENSE file or at http://eupl12.zgo.at
+// Copyright © 2019 Martin Tournoij – This file is part of GoatCounter and
+// published under the terms of a slightly modified EUPL v1.2 license, which can
+// be found in the LICENSE file or at https://license.goatcounter.com
 
 package acme
 
