ebb4dd2b7494881e6470dc5ece2d599328b611c4
Martin Tournoij <martin@arp242.net>
2020-07-20 16:45:40 +0800

    Prevent building GoatCounter with older Go versions

---
+++ old.go
@@ -0,0 +1,9 @@
+// +build !go1.13
+
+package goatcounter
+
+// Make sure people don't try to build GoatCounter with older versions of Go, as
+// that will introduce some runtime problems (e.g. using %w).
+func init() {
+	"You need Go 1.13 or newer to compile GoatCounter"
+}
