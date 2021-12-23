777ca19cd8978bb256dc886fd56f390aeae7c2e3
Martin Tournoij <martin@arp242.net>
2021-12-23 12:31:14 +0100

    Revert "Make TotalCounts query a bit faster"
    This reverts commit d7a11dc3caf93d00e2349a3463b89cc10415e3f3.
    
    Actually, this isn't faster at all; it's quite a bit slower on
    production. Was faster for me in my dev though, hmm 🤔

--- hit_list.go
+++ hit_list.go
@@ -455,20 +455,19 @@ type TotalCount struct {
 // per-hour, so we need to use the correct totals to make sure the percentage
 // calculations are accurate.
 func GetTotalCount(ctx context.Context, rng ztime.Range, pathFilter []int64, noEvents bool) (TotalCount, error) {
-	file := "load:hit_list.GetTotalCount"
-	if noEvents {
-		file += "-noEvents"
-	}
+	site := MustGetSite(ctx)
+	user := MustGetUser(ctx)
 
 	var t TotalCount
-	err := zdb.Get(ctx, &t, file, zdb.P{
-		"sqlite":    zdb.SQLDialect(ctx) == zdb.DialectSQLite,
-		"site":      MustGetSite(ctx).ID,
+	err := zdb.Get(ctx, &t, "load:hit_list.GetTotalCount", zdb.P{
+		"site":      site.ID,
 		"start":     rng.Start,
 		"end":       rng.End,
+		"start_utc": rng.Start.In(user.Settings.Timezone.Location),
+		"end_utc":   rng.End.In(user.Settings.Timezone.Location),
 		"filter":    pathFilter,
-		"tz":        MustGetUser(ctx).Settings.Timezone.Offset(),
 		"no_events": noEvents,
+		"tz":        user.Settings.Timezone.Offset(),
 	})
 	return t, errors.Wrap(err, "GetTotalCount")
 }
