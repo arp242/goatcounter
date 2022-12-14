38a27dd729823f727beb4f23b3fe70fa2f60e516
Martin Tournoij <martin@arp242.net>
2022-12-14 18:57:51 +0100

    Also fix test

--- acme/acme_test.go
+++ acme/acme_test.go
@@ -18,24 +18,26 @@ func TestSetup(t *testing.T) {
 	tests := []struct {
 		flag string
 
-		wantTLS  bool
-		wantACME bool
-		wantFlag uint8
+		wantTLS    bool
+		wantACME   bool
+		wantFlag   uint8
+		wantSecure bool
 	}{
 		// No TLS.
-		{"", false, false, 0},
-		{"http", false, false, 0},
+		{"", false, false, 0, false},
+		{"http", false, false, 0, false},
 
 		//flagTLS = map[bool]string{true: "none", false: "acme"}[dev]
 
-		{"acme,http", false, true, 0},                 // saas default
-		{"acme,rdr", true, true, zhttp.ServeRedirect}, // serve default
+		{"acme,http", false, true, 0, false}, // saas default
+		{"acme,proxy", false, true, 0, true},
+		{"acme,rdr", true, true, zhttp.ServeRedirect, true}, // serve default
 
-		{"acme:some/dir,rdr", true, true, zhttp.ServeRedirect},
+		{"acme:some/dir,rdr", true, true, zhttp.ServeRedirect, true},
 
-		{"acme,testdata/test.pem", true, true, 0},
-		{"testdata/test.pem", true, false, 0},
-		{"rdr,testdata/test.pem", true, false, zhttp.ServeRedirect},
+		{"acme,testdata/test.pem", true, true, 0, true},
+		{"testdata/test.pem", true, false, 0, true},
+		{"rdr,testdata/test.pem", true, false, zhttp.ServeRedirect, true},
 	}
 
 	for _, tt := range tests {
@@ -43,7 +45,7 @@ func TestSetup(t *testing.T) {
 			defer Reset()
 			ctx := gctest.DB(t)
 
-			tlsC, acmeH, haveFlag := Setup(zdb.MustGetDB(ctx), tt.flag, true)
+			tlsC, acmeH, haveFlag, haveSecure := Setup(zdb.MustGetDB(ctx), tt.flag, true)
 			haveTLS := tlsC != nil
 			haveACME := acmeH != nil
 
@@ -58,6 +60,9 @@ func TestSetup(t *testing.T) {
 			}
 			if haveFlag != tt.wantFlag {
 				t.Errorf("have flag %d; want %d", haveFlag, tt.wantFlag)
+			}
+			if haveSecure != tt.wantSecure {
+				t.Errorf("secure %v; want %v", haveSecure, tt.wantSecure)
 			}
 		})
 	}
