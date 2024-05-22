77e7e59fedc72a06a783c1f8c392859a456509c5
Martin Tournoij <martin@arp242.net>
2024-05-23 00:34:15 +0100

    Temporary fix for my own site

--- db/query/ref.ListRefsByPathID.sql
+++ db/query/ref.ListRefsByPathID.sql
@@ -11,7 +11,9 @@ with x as (
 select
 	x.count,
 	refs.ref_scheme as ref_scheme,
-	refs.ref        as name
+	-- This coalesce shouldn't be needed, and is just here to fix a borked
+	-- migration on my own site until such time I can properly fix this.
+	coalesce(refs.ref, '') as name
 from x
 left join refs using (ref_id)
 order by count desc, ref
