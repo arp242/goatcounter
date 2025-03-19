a66ebb35441ce67d0b83104e327c94b22133c744
Martin Tournoij <martin@arp242.net>
2025-03-19 00:16:46 +0000

    Remove Copyright/license headers
    Doesn't add that much value IMO.
    
    Also fix link in LICENCE.
    
    Fixes #747

--- public/script.js
+++ public/script.js
@@ -1,7 +1,3 @@
-// Copyright © 2019 Martin Tournoij – This file is part of GoatCounter and
-// published under the terms of a slightly modified EUPL v1.2 license, which can
-// be found in the LICENSE file or at https://license.goatcounter.com
-
 (function() {
 	var init = function() {
 		setup_imgzoom()
--- widgets/total_pages.go
+++ widgets/total_pages.go
@@ -1,7 +1,3 @@
-// Copyright © Martin Tournoij – This file is part of GoatCounter and published
-// under the terms of a slightly modified EUPL v1.2 license, which can be found
-// in the LICENSE file or at https://license.goatcounter.com
-
 package widgets
 
 import (
