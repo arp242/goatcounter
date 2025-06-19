arp242/goatcounter

commit db292331e86f38513f0d0b05ef70e068cabca924

Strip trailing slash from visitor counter
Fixes #796

handlers/vcounter.go
@@ -104,6 +104,7 @@ func (h vcounter) counter(w http.ResponseWriter, r *http.Request) error {
 		noBranding = r.URL.Query().Get("no_branding") != ""
 		style      = r.URL.Query().Get("style")
 	)
+	path = "/" + strings.Trim(path, "/")
 
 	if ext == "json" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")

commit 6897aed604c07c5b21bffb53745a93c68b502eea

Make sure /counter/TOTAL works
That broke since I fixed it for events in 0a1161ff, which was a
regression from db292331.

handlers/vcounter.go
@@ -104,23 +104,27 @@ func (h vcounter) counter(w http.ResponseWriter, r *http.Request) error {
 		noBranding = r.URL.Query().Get("no_branding") != ""
 		style      = r.URL.Query().Get("style")
 	)
-
 	// Sanitize slashes, but only if we can't find the path (so events work).
-	var p goatcounter.Path
-	err := p.ByPath(r.Context(), path)
-	if err != nil {
-		if !zdb.ErrNoRows(err) {
-			return err
+	if !total {
+		var p goatcounter.Path
+		err := p.ByPath(r.Context(), path)
+		if err != nil {
+			if !zdb.ErrNoRows(err) {
+				return err
+			}
+			path = "/" + strings.Trim(path, "/")
 		}
-		path = "/" + strings.Trim(path, "/")
 	}
 
 	if ext == "json" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 	}
 
-	var rng ztime.Range
-	startArg := r.URL.Query().Get("start")
+	var (
+		rng      ztime.Range
+		err      error
+		startArg = r.URL.Query().Get("start")
+	)
 	if startArg != "" {
 		switch startArg {
 		case "week":