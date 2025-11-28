arp242/goatcounter

commit db6c16759ac70c4cdeda7e8bca02359445c208e3

Use appropiate int types instead of int64 everywhere
GoatCounter always used int64s because back in 2018/2019 when I started
this, it was just so much easier. But now that we have type parameters,
using a more appropiate int type is a lot more easier.

Only hits.hit_id is int64, the rest is int32.

site.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"path"
-	"strconv"
 	"strings"
 	"time"
 
@@ -14,6 +13,7 @@ import (
 	"zgo.at/zstd/zcrypto"
 	"zgo.at/zstd/znet"
 	"zgo.at/zstd/zslice"
+	"zgo.at/zstd/zstrconv"
 	"zgo.at/zstd/zstring"
 	"zgo.at/zstd/ztime"
 )
@@ -29,9 +29,11 @@ var reserved = []string{
 var statTables = []string{"hit_stats", "system_stats", "browser_stats",
 	"location_stats", "language_stats", "size_stats"}
 
+type SiteID int32
+
 type Site struct {
-	ID     int64  `db:"site_id" json:"id,readonly"`
-	Parent *int64 `db:"parent" json:"parent,readonly"`
+	ID     SiteID  `db:"site_id" json:"id,readonly"`
+	Parent *SiteID `db:"parent" json:"parent,readonly"`
 
 	// Custom domain, e.g. "stats.example.com".
 	//
@@ -184,7 +186,7 @@ func (s *Site) Insert(ctx context.Context) error {
 		return err
 	}
 
-	s.ID, err = zdb.InsertID(ctx, "site_id", `insert into sites (
+	s.ID, err = zdb.InsertID[SiteID](ctx, "site_id", `insert into sites (
 		parent, code, cname, link_domain, settings, user_defaults, created_at, first_hit_at, cname_setup_at) values (?)`,
 		[]any{s.Parent, s.Code, s.Cname, s.LinkDomain, s.Settings, s.UserDefaults, s.CreatedAt, s.CreatedAt, s.CnameSetupAt})
 	if err != nil && zdb.ErrUnique(err) {
@@ -215,7 +217,7 @@ func (s *Site) Update(ctx context.Context) error {
 	return nil
 }
 
-func (s *Site) UpdateParent(ctx context.Context, newParent *int64) error {
+func (s *Site) UpdateParent(ctx context.Context, newParent *SiteID) error {
 	if s.ID == 0 {
 		return errors.New("ID == 0")
 	}
@@ -375,7 +377,7 @@ func (s *Site) Delete(ctx context.Context, deleteChildren bool) error {
 	})
 }
 
-func (s Site) Undelete(ctx context.Context, id int64) error {
+func (s Site) Undelete(ctx context.Context, id SiteID) error {
 	s.State = StateActive
 	s.ID = id
 	err := zdb.Exec(ctx, `update sites set state = ? where site_id = ?`, StateActive, id)
@@ -389,9 +391,9 @@ func (s Site) Undelete(ctx context.Context, id int64) error {
 
 // Exists checks if this site already exists, based on either the Cname or Code
 // field.
-func (s Site) Exists(ctx context.Context) (int64, error) {
+func (s Site) Exists(ctx context.Context) (SiteID, error) {
 	var (
-		id     int64
+		id     SiteID
 		query  = `select site_id from sites where lower(code) = lower($1) and site_id != $2 limit 1`
 		params = []any{s.Code, s.ID}
 	)
@@ -408,7 +410,7 @@ func (s Site) Exists(ctx context.Context) (int64, error) {
 }
 
 // ByID gets a site by ID.
-func (s *Site) ByID(ctx context.Context, id int64) error {
+func (s *Site) ByID(ctx context.Context, id SiteID) error {
 	err := s.ByIDState(ctx, id, StateActive)
 	if err != nil {
 		return fmt.Errorf("Site.ByID: %w", errors.Unwrap(err))
@@ -417,7 +419,7 @@ func (s *Site) ByID(ctx context.Context, id int64) error {
 }
 
 // ByIDState gets a site by ID and state. This may return deleted sites.
-func (s *Site) ByIDState(ctx context.Context, id int64, state string) error {
+func (s *Site) ByIDState(ctx context.Context, id SiteID, state string) error {
 	ss, ok := cacheSites(ctx).Get(id)
 	if ok {
 		*s = *ss
@@ -479,7 +481,7 @@ func (s *Site) ByHost(ctx context.Context, host string) error {
 
 // Find a site: by ID if ident is a number, or by host if it's not.
 func (s *Site) Find(ctx context.Context, ident string) error {
-	id, err := strconv.ParseInt(ident, 10, 64)
+	id, err := zstrconv.ParseInt[SiteID](ident, 10)
 	if err == nil {
 		return errors.Wrap(s.ByID(ctx, id), "Site.Find")
 	}
@@ -553,7 +555,7 @@ func (s Site) LinkDomainURL(withProto bool, paths ...string) string {
 }
 
 // IDOrParent gets this site's ID or the parent ID if that's set.
-func (s Site) IDOrParent() int64 {
+func (s Site) IDOrParent() SiteID {
 	if s.Parent != nil {
 		return *s.Parent
 	}
@@ -584,7 +586,7 @@ func (s Site) DeleteOlderThan(ctx context.Context, days int) error {
 	return zdb.TX(ctx, func(ctx context.Context) error {
 		ival := Interval(ctx, days)
 
-		var pathIDs []int64
+		var pathIDs []SiteID
 		err := zdb.Select(ctx, &pathIDs, `/* Site.DeleteOlderThan */
 			select path_id from hit_counts where site_id=$1 and hour < `+ival+` group by path_id`, s.ID)
 		if err != nil {
@@ -613,7 +615,7 @@ func (s Site) DeleteOlderThan(ctx context.Context, days int) error {
 		}
 
 		if len(pathIDs) > 0 {
-			var remainPath []int64
+			var remainPath []SiteID
 			err := zdb.Select(ctx, &remainPath, `/* Site.DeleteOlderThan */
 				select path_id from hit_counts where site_id=? and path_id in (?)`,
 				s.ID, pathIDs)
@@ -660,7 +662,7 @@ func (s *Sites) ListSubs(ctx context.Context) error {
 }
 
 // ForAccount gets all sites associated with an account.
-func (s *Sites) ForAccount(ctx context.Context, accountID int64) error {
+func (s *Sites) ForAccount(ctx context.Context, accountID SiteID) error {
 	err := zdb.Select(ctx, s, `/* Sites.ForThisAccount */
 		select * from sites
 		where state=$1 and (parent=$2 or site_id=$2) or (
@@ -720,10 +722,10 @@ func (s *Sites) Find(ctx context.Context, ident []string) error {
 }
 
 // IDs gets a list of all IDs for these sites.
-func (s *Sites) IDs() []int64 {
-	ids := make([]int64, 0, len(*s))
+func (s *Sites) IDs() []int32 {
+	ids := make([]int32, 0, len(*s))
 	for _, ss := range *s {
-		ids = append(ids, ss.ID)
+		ids = append(ids, int32(ss.ID))
 	}
 	return ids
 }
@@ -743,7 +745,7 @@ func (s *Sites) Delete(ctx context.Context, deleteChildren bool) error {
 }
 
 // ListIDs lists all sites with the given IDs.
-func (s *Sites) ListIDs(ctx context.Context, ids ...int64) error {
+func (s *Sites) ListIDs(ctx context.Context, ids ...SiteID) error {
 	if len(ids) == 0 {
 		return nil
 	}

import.go
@@ -76,7 +76,7 @@ func ImportGA(ctx context.Context, fp io.Reader) error {
 
 	var (
 		siteID = MustGetSite(ctx).ID
-		paths  = make(map[string]int64)
+		paths  = make(map[string]PathID)
 	)
 	for p := range pages {
 		pp := Path{

api_token.go
@@ -26,10 +26,12 @@ const (
 	APIPermStats                     // 64
 )
 
+type APITokenID int32
+
 type APIToken struct {
-	ID     int64 `db:"api_token_id" json:"-"`
-	SiteID int64 `db:"site_id" json:"-"`
-	UserID int64 `db:"user_id" json:"-"`
+	ID     APITokenID `db:"api_token_id" json:"-"`
+	SiteID SiteID     `db:"site_id" json:"-"`
+	UserID UserID     `db:"user_id" json:"-"`
 
 	Name        string         `db:"name" json:"name"`
 	Token       string         `db:"token" json:"-"`
@@ -150,7 +152,7 @@ func (t *APIToken) Insert(ctx context.Context) error {
 		return err
 	}
 
-	t.ID, err = zdb.InsertID(ctx, "api_token_id",
+	t.ID, err = zdb.InsertID[APITokenID](ctx, "api_token_id",
 		`insert into api_tokens (site_id, user_id, name, token, permissions, created_at) values (?)`,
 		[]any{t.SiteID, GetUser(ctx).ID, t.Name, t.Token, t.Permissions, t.CreatedAt})
 	return errors.Wrap(err, "APIToken.Insert")
@@ -191,7 +193,7 @@ func (t *APIToken) UpdateLastUsed(ctx context.Context) error {
 	return errors.Wrap(err, "APIToken.UpdateLastUsed")
 }
 
-func (t *APIToken) ByID(ctx context.Context, id int64) error {
+func (t *APIToken) ByID(ctx context.Context, id APITokenID) error {
 	return errors.Wrapf(zdb.Get(ctx, t, `/* APIToken.ByID */
 		select * from api_tokens where api_token_id=$1 and site_id=$2`,
 		id, MustGetSite(ctx).ID), "APIToken.ByID %d", id)
@@ -230,10 +232,10 @@ func (t *APITokens) Find(ctx context.Context, ident []string) error {
 }
 
 // IDs gets a list of all IDs for these API tokens.
-func (t *APITokens) IDs() []int64 {
-	ids := make([]int64, 0, len(*t))
+func (t *APITokens) IDs() []int32 {
+	ids := make([]int32, 0, len(*t))
 	for _, tt := range *t {
-		ids = append(ids, tt.ID)
+		ids = append(ids, int32(tt.ID))
 	}
 	return ids
 }

campaign.go
@@ -8,10 +8,12 @@ import (
 	"zgo.at/zvalidate"
 )
 
+type CampaignID int32
+
 type Campaign struct {
-	ID     int64  `db:"campaign_id" json:"campaign_id"`
-	SiteID int64  `db:"site_id" json:"site_id"`
-	Name   string `db:"name" json:"name"`
+	ID     CampaignID `db:"campaign_id" json:"campaign_id"`
+	SiteID SiteID     `db:"site_id" json:"site_id"`
+	Name   string     `db:"name" json:"name"`
 }
 
 func (c *Campaign) Defaults(ctx context.Context) {}
@@ -33,7 +35,7 @@ func (c *Campaign) Insert(ctx context.Context) error {
 		return errors.Wrap(err, "Campaign.Insert")
 	}
 
-	c.ID, err = zdb.InsertID(ctx, "campaign_id",
+	c.ID, err = zdb.InsertID[CampaignID](ctx, "campaign_id",
 		`insert into campaigns (site_id, name) values (?, ?)`, MustGetSite(ctx).ID, c.Name)
 	if err != nil {
 		return errors.Wrap(err, "Campaign.Insert")

cmd/goatcounter/dashboard.go
@@ -7,7 +7,6 @@ import (
 	"math"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -16,6 +15,7 @@ import (
 	"zgo.at/termtext"
 	"zgo.at/zli"
 	"zgo.at/zstd/zint"
+	"zgo.at/zstd/zstrconv"
 	"zgo.at/zstd/zstring"
 	"zgo.at/zstd/ztime"
 )
@@ -104,7 +104,7 @@ func parseRange(rangeFlag string) (ztime.Range, error) {
 	}
 
 	// -range 30 for last 30 days.
-	n, err := strconv.ParseInt(rangeFlag, 0, 64)
+	n, err := zstrconv.ParseInt[int64](rangeFlag, 0)
 	if err == nil {
 		rng.Start = now.AddPeriod(int(-n), ztime.Day).Time
 		return rng, nil

cmd/goatcounter/db.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"slices"
-	"strconv"
 	"strings"
 
 	"golang.org/x/text/language"
@@ -18,6 +17,7 @@ import (
 	"zgo.at/zli"
 	"zgo.at/zstd/zcrypto"
 	"zgo.at/zstd/zint"
+	"zgo.at/zstd/zstrconv"
 	"zgo.at/zstd/zstring"
 	"zgo.at/zstd/ztype"
 	"zgo.at/zvalidate"
@@ -347,7 +347,7 @@ Use "goatcounter help db" for the full documentation.`
 type (
 	findMany interface {
 		Find(context.Context, []string) error
-		IDs() []int64
+		IDs() []int32
 		Delete(context.Context, bool) error
 	}
 	stringFlag interface {
@@ -656,7 +656,6 @@ func cmdDBDelete(f zli.Flags, cmd string, dbConnect *string, debug []string, cre
 	if err != nil {
 		return err
 	}
-
 	return finder.Delete(ctx, *force)
 }
 
@@ -972,8 +971,8 @@ func cmdDBAPITokenCreate(ctx context.Context,
 		return v
 	}
 
-	var siteID int64
-	findUserID, _ := strconv.ParseInt(findUser, 10, 64)
+	var siteID goatcounter.SiteID
+	findUserID, _ := zstrconv.ParseInt[goatcounter.UserID](findUser, 10)
 	err := zdb.Get(ctx, &siteID,
 		`select site_id from users where user_id = $1 or email = $2`,
 		findUserID, findUser)

cmd/goatcounter/import_test.go
@@ -86,7 +86,7 @@ func runImportClean(ctx context.Context, t *testing.T) func() {
 			t.Fatal(err)
 		}
 
-		var paths []int64
+		var paths []goatcounter.PathID
 		err = zdb.Select(ctx, &paths, `select path_id from paths`)
 		if err != nil {
 			t.Fatal(err)

context.go
@@ -159,7 +159,7 @@ func MustGetUser(ctx context.Context) *User {
 func CopyContextValues(ctx context.Context) context.Context {
 	n := zdb.WithDB(context.Background(), zdb.MustGetDB(ctx))
 	if c := ctx.Value(keyCacheSites); c != nil {
-		n = context.WithValue(n, keyCacheSites, c.(*zcache.Cache[int64, *Site]))
+		n = context.WithValue(n, keyCacheSites, c.(*zcache.Cache[SiteID, *Site]))
 	}
 	if c := ctx.Value(keyCacheUA); c != nil {
 		n = context.WithValue(n, keyCacheUA, c.(*zcache.Cache[string, UserAgent]))
@@ -189,7 +189,7 @@ func CopyContextValues(ctx context.Context) context.Context {
 		n = context.WithValue(n, keyChangedTitles, c.(*zcache.Cache[string, []string]))
 	}
 	if c := ctx.Value(keyCacheSitesProxy); c != nil {
-		n = context.WithValue(n, keyCacheSitesProxy, c.(*zcache.Proxy[string, int64, *Site]))
+		n = context.WithValue(n, keyCacheSitesProxy, c.(*zcache.Proxy[string, SiteID, *Site]))
 	}
 	if c := Config(ctx); c != nil {
 		n = context.WithValue(n, keyConfig, c)
@@ -219,9 +219,9 @@ func NewContext(ctx context.Context, db zdb.DB) context.Context {
 }
 
 func NewCache(ctx context.Context) context.Context {
-	s := zcache.New[int64, *Site](24*time.Hour, 1*time.Hour)
+	s := zcache.New[SiteID, *Site](24*time.Hour, 1*time.Hour)
 	ctx = context.WithValue(ctx, keyCacheSites, s)
-	ctx = context.WithValue(ctx, keyCacheSitesProxy, zcache.NewProxy[string, int64, *Site](s))
+	ctx = context.WithValue(ctx, keyCacheSitesProxy, zcache.NewProxy[string, SiteID, *Site](s))
 
 	ctx = context.WithValue(ctx, keyCacheUA, zcache.New[string, UserAgent](1*time.Hour, 5*time.Minute))
 	ctx = context.WithValue(ctx, keyCacheBrowsers, zcache.New[string, Browser](1*time.Hour, 5*time.Minute))
@@ -246,11 +246,11 @@ func Config(ctx context.Context) *GlobalConfig {
 	return &GlobalConfig{}
 }
 
-func cacheSites(ctx context.Context) *zcache.Cache[int64, *Site] {
+func cacheSites(ctx context.Context) *zcache.Cache[SiteID, *Site] {
 	if c := ctx.Value(keyCacheSites); c != nil {
-		return c.(*zcache.Cache[int64, *Site])
+		return c.(*zcache.Cache[SiteID, *Site])
 	}
-	return zcache.New[int64, *Site](0, 0)
+	return zcache.New[SiteID, *Site](0, 0)
 }
 func cacheUA(ctx context.Context) *zcache.Cache[string, UserAgent] {
 	if c := ctx.Value(keyCacheUA); c != nil {
@@ -306,9 +306,9 @@ func cacheChangedTitles(ctx context.Context) *zcache.Cache[string, []string] {
 	}
 	return zcache.New[string, []string](0, 0)
 }
-func cacheSitesHost(ctx context.Context) *zcache.Proxy[string, int64, *Site] {
+func cacheSitesHost(ctx context.Context) *zcache.Proxy[string, SiteID, *Site] {
 	if c := ctx.Value(keyCacheSitesProxy); c != nil {
-		return c.(*zcache.Proxy[string, int64, *Site])
+		return c.(*zcache.Proxy[string, SiteID, *Site])
 	}
-	return zcache.NewProxy[string, int64, *Site](zcache.New[int64, *Site](0, 0))
+	return zcache.NewProxy[string, SiteID, *Site](zcache.New[SiteID, *Site](0, 0))
 }

commit 8a8fa6f92fedb89f284445d0bb89d8e6628fed65

Unmarshal SiteIDs to fix import command
Previously:

    % goatcounter import -site=http://localhost:8081 a.csv
    goatcounter: json: cannot unmarshal array into Go struct field APIToken.token.sites of type goatcounter.SiteIDs

Regression from db6c1675

Also give a better error if you forget to add -site:

    % goatcounter import a.csv
    goatcounter: Get "https:///api/v0/me": http: no Host in request URL

cmd/goatcounter/import.go
@@ -237,6 +237,9 @@ func cmdImport(f zli.Flags, ready chan<- struct{}, stop chan struct{}) error {
 		if len(files) > 1 {
 			return fmt.Errorf("can only specify one filename")
 		}
+		if site == "" {
+			return fmt.Errorf("-site needs to be set")
+		}
 
 		var fp io.ReadCloser
 		if files[0] == "-" {

site.go
@@ -36,6 +36,15 @@ type (
 	SiteIDs []SiteID
 )
 
+func (s *SiteIDs) UnmarshalJSON(data []byte) error {
+	var tmp []SiteID
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	*s = tmp
+	return nil
+}
+
 // func (s SiteIDs) MarshalText() ([]byte, error) { return json.Marshal(s) }
 func (s *SiteIDs) UnmarshalText(v []byte) error {
 	n, err := zstrconv.ParseInt[SiteID](string(v), 10)