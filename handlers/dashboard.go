arp242/goatcounter

commit ee918e29e169113be7f1f66f36acbe04d762fc49

Refactor Daily bool to Group enum
Instead of just having a bool to turn daily on/off, add a Group enum to
explicitly set "view by {hour,day,...}".

This will allow adding more options down the line, such as grouping by
week or month.

handlers/dashboard.go
@@ -62,11 +62,8 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 	if _, ok := q["filter"]; ok {
 		view.Filter = q.Get("filter")
 	}
-	if _, ok := q["daily"]; ok {
-		view.Daily = q.Get("daily") == "on" || q.Get("daily") == "true"
-	}
-	var forcedDaily bool
-	view.Daily, forcedDaily = getDaily(r, rng)
+	var forcedGroup bool
+	view.Group, forcedGroup = getGroup(r, rng)
 
 	// Get path IDs to filter first, as they're used by the widgets.
 	var (
@@ -106,8 +103,8 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 
 	args := widgets.Args{
 		Rng:         rng,
-		Daily:       view.Daily,
-		ForcedDaily: forcedDaily,
+		Group:       view.Group,
+		ForcedGroup: forcedGroup,
 		ShowRefs:    showRefs,
 	}
 
@@ -259,14 +256,14 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 		ShowRefs    goatcounter.PathID
 		Period      ztime.Range
 		PathFilter  []goatcounter.PathID
-		ForcedDaily bool
+		ForcedGroup bool
 		Widgets     widgets.List
 		View        goatcounter.View
 		Total       int
 		TotalUTC    int
 		ConnectID   zint.Uint128
 	}{newGlobals(w, r), cd, subs, showRefs, rng,
-		args.PathFilter, forcedDaily, wid, view, shared.Total, shared.TotalUTC,
+		args.PathFilter, forcedGroup, wid, view, shared.Total, shared.TotalUTC,
 		connectID})
 }
 
@@ -315,7 +312,7 @@ func (h backend) loadWidget(w http.ResponseWriter, r *http.Request) error {
 		p := wid.(*widgets.Pages)
 
 		args.RowsOnly = true
-		args.Args.Daily, args.Args.ForcedDaily = getDaily(r, rng)
+		args.Args.Group, args.Args.ForcedGroup = getGroup(r, rng)
 
 		if key != "" {
 			p.RefsForPath, _ = zstrconv.ParseInt[goatcounter.PathID](key, 10)
@@ -428,16 +425,19 @@ func getPeriod(w http.ResponseWriter, r *http.Request, site *goatcounter.Site, u
 	return rng.From(rng.Start).To(rng.End).UTC(), nil
 }
 
-func getDaily(r *http.Request, rng ztime.Range) (daily bool, forced bool) {
+func getGroup(r *http.Request, rng ztime.Range) (g goatcounter.Group, forced bool) {
 	// Force daily view for large timespans and force hourly for very short
 	// ones, as it looks horrible otherwise.
 	if d := rng.End.Sub(rng.Start).Hours() / 24; d >= 90 {
-		return true, true
+		return goatcounter.GroupDaily, true
 	} else if d <= 6 {
-		return false, true
+		return goatcounter.GroupHourly, true
 	}
-	d := strings.ToLower(r.URL.Query().Get("daily"))
-	return d == "on" || d == "true", false
+	switch strings.ToLower(r.URL.Query().Get("group")) {
+	case "day":
+		return goatcounter.GroupDaily, false
+	}
+	return goatcounter.GroupHourly, false
 }
 
 func getPathFilter(v *zvalidate.Validator, r *http.Request) []goatcounter.PathID {

cron/email_reports.go
@@ -128,7 +128,7 @@ func reportText(ctx context.Context, site goatcounter.Site, user goatcounter.Use
 	subject = fmt.Sprintf("Your GoatCounter report for %s", args.DisplayDate)
 
 	{ // Get overview of paths.
-		_, _, err := args.Pages.List(ctx, rng, nil, nil, 10, true)
+		_, _, err := args.Pages.List(ctx, rng, nil, nil, 10, goatcounter.GroupDaily)
 		if err != nil {
 			return nil, nil, "", err
 		}
@@ -137,7 +137,7 @@ func reportText(ctx context.Context, site goatcounter.Site, user goatcounter.Use
 			return nil, nil, "", nil
 		}
 
-		_, err = args.Total.Totals(ctx, rng, nil, true, true)
+		_, err = args.Total.Totals(ctx, rng, nil, goatcounter.GroupDaily, true)
 		if err != nil {
 			return nil, nil, "", err
 		}

cron/hit_stat_test.go
@@ -32,7 +32,7 @@ func TestHitStats(t *testing.T) {
 		var stats goatcounter.HitLists
 		display, more, err := stats.List(ctx,
 			ztime.NewRange(now.Add(-1*time.Hour)).To(now.Add(1*time.Hour)),
-			nil, nil, 10, false)
+			nil, nil, 10, goatcounter.GroupHourly)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -107,7 +107,7 @@ func TestHitStatsNoCollect(t *testing.T) {
 		var stats goatcounter.HitLists
 		display, more, err := stats.List(ctx,
 			ztime.NewRange(now.Add(-1*time.Hour)).To(now.Add(1*time.Hour)),
-			nil, nil, 10, false)
+			nil, nil, 10, goatcounter.GroupHourly)
 		if err != nil {
 			t.Fatal(err)
 		}

cron/tasks_test.go
@@ -50,7 +50,7 @@ func TestDataRetention(t *testing.T) {
 	var stats goatcounter.HitLists
 	display, more, err := stats.List(ctx,
 		ztime.NewRange(past.Add(-1*24*time.Hour)).To(now),
-		nil, nil, 10, false)
+		nil, nil, 10, goatcounter.GroupHourly)
 	if err != nil {
 		t.Fatal(err)
 	}

handlers/api.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -931,9 +932,19 @@ func (h api) hits(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
+	group := goatcounter.GroupHourly
+	if args.Daily {
+		// TODO: ideally would like to refactor this to Group, or maybe just
+		// remove. Lets see if anyone is using it.
+		m := metrics.Start("api-hits-daily")
+		m.AddTag(strconv.FormatInt(int64(Site(r.Context()).ID), 10))
+		m.Done()
+		group = goatcounter.GroupDaily
+	}
+
 	var pages goatcounter.HitLists
 	tdu, more, err := pages.List(r.Context(), ztime.NewRange(args.Start).To(args.End),
-		includeIDs, excludeIDs, args.Limit, args.Daily)
+		includeIDs, excludeIDs, args.Limit, group)
 	if err != nil {
 		return err
 	}
@@ -1094,7 +1105,7 @@ func (h api) countTotal(w http.ResponseWriter, r *http.Request) error {
 	func() {
 		defer wg.Done()
 		defer log.Recover(r.Context())
-		_, oErr = total.Totals(r.Context(), rng, includeIDs, true, false)
+		_, oErr = total.Totals(r.Context(), rng, includeIDs, goatcounter.GroupDaily, false)
 	}()
 	wg.Wait()
 	if tcErr != nil {

hit_list.go
@@ -14,6 +14,25 @@ import (
 	"zgo.at/zstd/ztime"
 )
 
+type Group uint8
+
+func (g Group) Hourly() bool { return g == GroupHourly }
+func (g Group) Daily() bool  { return g == GroupDaily }
+func (g Group) String() string {
+	switch g {
+	case GroupDaily:
+		return "day"
+	}
+	return "hour"
+}
+
+const (
+	GroupHourly = Group(iota)
+	GroupDaily
+)
+
+var Groups = []Group{GroupHourly, GroupDaily}
+
 type HitList struct {
 	// Number of visitors for the selected date range.
 	Count int `db:"count" json:"count"`
@@ -97,8 +116,9 @@ var allDays = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 
 // List the top paths for this site in the given time period.
 func (h *HitLists) List(
-	ctx context.Context, rng ztime.Range, pathFilter, exclude []PathID, limit int, daily bool,
+	ctx context.Context, rng ztime.Range, pathFilter, exclude []PathID, limit int, group Group,
 ) (int, bool, error) {
+
 	site := MustGetSite(ctx)
 	user := MustGetUser(ctx)
 
@@ -177,7 +197,7 @@ func (h *HitLists) List(
 
 	// Add total and max.
 	var totalDisplay int
-	addTotals(hh, daily, &totalDisplay)
+	addTotals(hh, group, &totalDisplay)
 
 	return totalDisplay, more, nil
 }
@@ -188,7 +208,7 @@ func (h *HitLists) List(
 const PathTotals = "TOTAL "
 
 // Totals gets the data for the "Totals" chart/widget.
-func (h *HitList) Totals(ctx context.Context, rng ztime.Range, pathFilter []PathID, daily, noEvents bool) (int, error) {
+func (h *HitList) Totals(ctx context.Context, rng ztime.Range, pathFilter []PathID, group Group, noEvents bool) (int, error) {
 	site := MustGetSite(ctx)
 	user := MustGetUser(ctx)
 
@@ -233,7 +253,7 @@ func (h *HitList) Totals(ctx context.Context, rng ztime.Range, pathFilter []Path
 	max := 0
 	for _, v := range stats {
 		totalst.Stats = append(totalst.Stats, v)
-		if !daily {
+		if group.Hourly() {
 			for _, x := range v.Hourly {
 				if x > max {
 					max = x
@@ -250,12 +270,12 @@ func (h *HitList) Totals(ctx context.Context, rng ztime.Range, pathFilter []Path
 	fillBlankDays(hh, rng)
 	applyOffset(hh, user.Settings.Timezone)
 
-	if daily {
+	if group.Daily() {
 		for i := range hh[0].Stats {
 			for _, n := range hh[0].Stats[i].Hourly {
 				hh[0].Stats[i].Daily += n
 			}
-			if daily && hh[0].Stats[i].Daily > max {
+			if hh[0].Stats[i].Daily > max {
 				max = hh[0].Stats[i].Daily
 			}
 		}
@@ -376,18 +396,18 @@ func fillBlankDays(hh HitLists, rng ztime.Range) {
 	}
 }
 
-func addTotals(hh HitLists, daily bool, totalDisplay *int) {
+func addTotals(hh HitLists, group Group, totalDisplay *int) {
 	for i := range hh {
 		for j := range hh[i].Stats {
 			for k := range hh[i].Stats[j].Hourly {
 				hh[i].Stats[j].Daily += hh[i].Stats[j].Hourly[k]
-				if !daily && hh[i].Stats[j].Hourly[k] > hh[i].Max {
+				if !group.Daily() && hh[i].Stats[j].Hourly[k] > hh[i].Max {
 					hh[i].Max = hh[i].Stats[j].Hourly[k]
 				}
 			}
 
 			hh[i].Count += hh[i].Stats[j].Daily
-			if daily && hh[i].Stats[j].Daily > hh[i].Max {
+			if group.Daily() && hh[i].Stats[j].Daily > hh[i].Max {
 				hh[i].Max = hh[i].Stats[j].Daily
 			}
 		}

hit_list_test.go
@@ -164,7 +164,7 @@ func TestHitListsList(t *testing.T) {
 			}
 
 			var stats HitLists
-			uniqueDisplay, more, err := stats.List(ctx, rng, pathsFilter, tt.inExclude, 2, false)
+			uniqueDisplay, more, err := stats.List(ctx, rng, pathsFilter, tt.inExclude, 2, GroupHourly)
 
 			have := fmt.Sprintf("%d %t %v", uniqueDisplay, more, err)
 			if have != tt.wantReturn {
@@ -291,7 +291,7 @@ func TestHitListTotals(t *testing.T) {
 		for i, filter := range [][]PathID{nil, []PathID{1}, []PathID{2}, []PathID{1, 2}} {
 			t.Run("", func(t *testing.T) {
 				var hs HitList
-				count, err := hs.Totals(ctx, rng, filter, false, false)
+				count, err := hs.Totals(ctx, rng, filter, GroupHourly, false)
 				if err != nil {
 					t.Fatal(err)
 				}
@@ -366,7 +366,7 @@ func TestHitListTotals(t *testing.T) {
 		for i, filter := range [][]PathID{nil, []PathID{1}, []PathID{2}, []PathID{1, 2}} {
 			t.Run("", func(t *testing.T) {
 				var hs HitList
-				count, err := hs.Totals(ctx, rng, filter, true, false)
+				count, err := hs.Totals(ctx, rng, filter, GroupDaily, false)
 				if err != nil {
 					t.Fatal(err)
 				}

public/backend.css
@@ -55,8 +55,9 @@ h3, h4     { margin-bottom: 0; }
 h3+p, h4+p { margin-top: 0; }
 h3.border  { border-bottom: 1px solid #666; margin-bottom: .2em; }
 
-form .err      { color: var(--form-error-text); display: block; }
-form .disabled { color:var(--pageviews-text); cursor:not-allowed;}
+form .err        { color:var(--form-error-text); display:block; }
+form .disabled,
+form *[disabled] { color:var(--pageviews-text); cursor:not-allowed;}
 
 span.red  { color: var(--form-error-text); }
 input.red { border: 1px solid var(--input-error-border) !important; }
@@ -278,7 +279,10 @@ tr.target .chart-left { display: block; }
 .period-year [value=year],
 .period-week-cur [value=week-cur],
 .period-month-cur [value=month-cur],
-.period-year-cur [value=year-cur] {
+.period-year-cur [value=year-cur],
+.group-hour [value=hour],
+.group-day [value=day],
+.group-week [value=week] {
     font-weight: bold;
     text-decoration: underline;
 }

commit cd0489f106c4a46434c4a21808a9885cd9c1d769

Fix loading "view by" from default view
Regression from ee918e29;; previously this was just a bool and we did:

    view.Daily, forcedDaily = getDaily(r, rng)

It returned false if it wasn't "forced" and that worked fine. But
changing it to an int/enum it became:

    view.Group, forcedGroup = getGroup(r, rng)

And it would just always return "hour" if not forced, which is obviously
not fine.

Fixes #837

handlers/dashboard.go
@@ -67,7 +67,7 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 		view.Filter = q.Get("filter")
 	}
 	var forcedGroup bool
-	view.Group, forcedGroup = getGroup(r, rng)
+	view.Group, forcedGroup = getGroup(r, view.Group, rng)
 
 	// Get path IDs to filter first, as they're used by the widgets.
 	var (
@@ -316,7 +316,7 @@ func (h backend) loadWidget(w http.ResponseWriter, r *http.Request) error {
 		p := wid.(*widgets.Pages)
 
 		args.RowsOnly = true
-		args.Args.Group, args.Args.ForcedGroup = getGroup(r, rng)
+		args.Args.Group, args.Args.ForcedGroup = getGroup(r, args.Args.Group, rng)
 
 		if key != "" {
 			p.RefsForPath, _ = zstrconv.ParseInt[goatcounter.PathID](key, 10)
@@ -429,7 +429,7 @@ func getPeriod(w http.ResponseWriter, r *http.Request, site *goatcounter.Site, u
 	return rng.From(rng.Start).To(rng.End).UTC(), nil
 }
 
-func getGroup(r *http.Request, rng ztime.Range) (g goatcounter.Group, forced bool) {
+func getGroup(r *http.Request, g goatcounter.Group, rng ztime.Range) (goatcounter.Group, bool) {
 	// Force daily view for large timespans and force hourly for very short
 	// ones, as it looks horrible otherwise.
 	if d := rng.End.Sub(rng.Start).Hours() / 24; d >= 90 {
@@ -438,10 +438,12 @@ func getGroup(r *http.Request, rng ztime.Range) (g goatcounter.Group, forced boo
 		return goatcounter.GroupHourly, true
 	}
 	switch strings.ToLower(r.URL.Query().Get("group")) {
+	case "hour":
+		return goatcounter.GroupHourly, false
 	case "day":
 		return goatcounter.GroupDaily, false
 	}
-	return goatcounter.GroupHourly, false
+	return g, false
 }
 
 func getPathFilter(v *zvalidate.Validator, r *http.Request) []goatcounter.PathID {

user.go
@@ -349,10 +349,8 @@ func (u *User) ByToken(ctx context.Context, token string) error {
 	if token == "" {
 		return sql.ErrNoRows
 	}
-
-	return errors.Wrap(zdb.Get(ctx, u,
-		`select * from users where login_token=$1`, token),
-		"User.ByToken")
+	err := zdb.Get(ctx, u, `select * from users where login_token=$1`, token)
+	return errors.Wrap(err, "User.ByToken")
 }
 
 // ByTokenAndSite gets a user by login token.
@@ -361,9 +359,9 @@ func (u *User) ByTokenAndSite(ctx context.Context, token string) error {
 		return sql.ErrNoRows
 	}
 
-	return errors.Wrap(zdb.Get(ctx, u,
-		`select * from users where login_token=$1 and site_id=$2`,
-		token, MustGetSite(ctx).IDOrParent()), "User.ByTokenAndSite")
+	err := zdb.Get(ctx, u, `select * from users where login_token=$1 and site_id=$2`,
+		token, MustGetSite(ctx).IDOrParent())
+	return errors.Wrap(err, "User.ByTokenAndSite")
 }
 
 // RequestReset generates a new password reset key.