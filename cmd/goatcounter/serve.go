arp242/goatcounter

commit 973955bc62ba144a3519b39a03d39f488d97793c

Support smtps:// URLs, add better email debugging
Support -smtp 'smtps://...' – previously only STARTTLS worked, but not
direct TLS connections.

Allow adding ?debug=1 to print all SMTP traffic to stderr, which can be
useful to debug the -smtp string.

Add page in Server Settings which can send a test email.

Fixes #825

cmd/goatcounter/serve.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -109,18 +110,15 @@ Flags:
 
   -automigrate Automatically run all pending migrations on startup.
 
-  -smtp        SMTP relay server, as URL (e.g. "smtp://user:pass@server").
+  -smtp        SMTP relay server, as URL (e.g. "smtp://user:pass@server:port").
+               TLS connections are supported via smtps:// or STARTTLS.
 
-               A special value of "stdout" will print emails to stdout without
-               actually sending them.  This is the default.
+               When the debug query parameter is present ("smtp://...?debug=1")
+               all client/server traffic will be written to stderr.
 
-               If this is an empty string (-smtp='') emails will be sent
-               without using a relay. This implementation is very simple and
-               deliverability will usually be bad (i.e. it will end up in the
-               spam box, or just be outright rejected). This usually requires
-               rDNS properly set up, and GoatCounter will *not* retry on
-               errors. Using a local smtp relay is almost always better unless
-               you really know what you're doing.
+               A special value of "stdout" or "stderr" will print emails to
+               stdout or stderr without actually sending them The default is
+               "stdout".
 
   -email-from  From: address in emails. Default: <user>@<hostname>
 
@@ -202,7 +200,7 @@ func cmdServe(f zli.Flags, ready chan<- struct{}, stop chan struct{}, saas bool)
 		dev          = f.Bool(false, "dev")
 		automigrate  = f.Bool(false, "automigrate")
 		listen       = f.String(":8080", "listen")
-		smtp         = f.String(blackmail.ConnectWriter, "smtp")
+		smtp         = f.String("stdout", "smtp")
 		flagTLS      = f.String("http", "tls")
 		errorsFlag   = f.String("", "errors")
 		from         = f.String("", "email-from")
@@ -235,10 +233,10 @@ func cmdServe(f zli.Flags, ready chan<- struct{}, stop chan struct{}, saas bool)
 	}
 
 	flagErrors(&v, errorsFlag.String())
-	flagEmail(&v, smtp.String())
+	mailer := flagEmail(&v, smtp.String())
 	geodb := setupGeo(&v, geodbFlag.String())
 	ratelimits := setupRatelimits(&v, ratelimit.String())
-	*from.Pointer() = flagFrom(&v, from.String(), domain.String())
+	*from.Pointer() = flagFrom(&v, saas, from.String(), domain.String())
 	domainCount, urlStatic := setupDomains(&v, saas, dev.Bool(), domain.Pointer(),
 		domainStatic.Pointer(), basePath.Pointer())
 
@@ -259,6 +257,7 @@ func cmdServe(f zli.Flags, ready chan<- struct{}, stop chan struct{}, saas bool)
 
 	ctx = z18n.With(ctx, z18n.NewBundle(language.English).Locale("en"))
 	ctx = geo.With(ctx, geodb)
+	ctx = blackmail.With(ctx, mailer)
 
 	if err := setupTpl(ctx, dev.Bool()); err != nil {
 		return err
@@ -474,11 +473,31 @@ func flagErrors(v *zvalidate.Validator, errors string) {
 	}
 }
 
-func flagEmail(v *zvalidate.Validator, smtp string) {
-	if smtp != blackmail.ConnectDirect && smtp != blackmail.ConnectWriter {
+func flagEmail(v *zvalidate.Validator, smtp string) blackmail.Mailer {
+	var (
+		m   blackmail.Mailer
+		err error
+	)
+	switch {
+	case strings.ToLower(smtp) == "stdout":
+		m = blackmail.NewWriter(os.Stdout)
+	case strings.ToLower(smtp) == "stderr":
+		m = blackmail.NewWriter(os.Stderr)
+	default:
 		v.URLLocal("-smtp", smtp)
+
+		var opt blackmail.RelayOptions
+		u, _ := url.Parse(smtp)
+		if u.Query().Has("debug") {
+			opt.Debug = os.Stderr
+		}
+		m, err = blackmail.NewRelay(smtp, &opt)
 	}
-	blackmail.DefaultMailer = blackmail.NewMailer(smtp)
+	if err != nil {
+		v.Append("-smtp", fmt.Sprintf("setting up mailer: %s", err))
+	}
+
+	return m
 }
 
 func setupGeo(v *zvalidate.Validator, geodbFlag string) *geoip2.Reader {
@@ -522,9 +541,9 @@ func setupRatelimits(v *zvalidate.Validator, ratelimit string) handlers.Ratelimi
 	return h
 }
 
-func flagFrom(v *zvalidate.Validator, from, domain string) string {
+func flagFrom(v *zvalidate.Validator, saas bool, from, domain string) string {
 	if from == "" {
-		if domain != "" { // saas only.
+		if saas && domain != "" { // saas only.
 			from = "support@" + znet.RemovePort(domain)
 		} else {
 			u, err := user.Current()

go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/BurntSushi/toml v1.5.0
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/teamwork/reload v1.4.2
-	zgo.at/blackmail v0.0.0-20241118175226-9a8f9442bb0d
+	zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615
 	zgo.at/errors v1.4.0
 	zgo.at/follow v0.0.0-20250412000721-fecf5127a123
 	zgo.at/gadget v1.0.0

go.sum
@@ -57,8 +57,8 @@ golang.org/x/text v0.31.0 h1:aC8ghyu4JhP8VojJ2lEHBnochRno1sgL6nEi9WGFGMM=
 golang.org/x/text v0.31.0/go.mod h1:tKRAlv61yKIjGGHX/4tP1LTbc13YSec1pxVEWXzfoeM=
 golang.org/x/tools v0.38.0 h1:Hx2Xv8hISq8Lm16jvBZ2VQf+RLmbd7wVUsALibYI/IQ=
 golang.org/x/tools v0.38.0/go.mod h1:yEsQ/d/YK8cjh0L6rZlY8tgtlKiBNTL14pGDJPJpYQs=
-zgo.at/blackmail v0.0.0-20241118175226-9a8f9442bb0d h1:j82yKhWF5jDs8i62JJBhbR2LLuDH7UCul5hmpUGDBHU=
-zgo.at/blackmail v0.0.0-20241118175226-9a8f9442bb0d/go.mod h1:GJGG5S0K6R1SxIvJuT+vQPVYF3uusvljesKv/E3/7hs=
+zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615 h1:AdTk8a17k7bJv2OIGnN6JHtqljSVltt0PG+jcLG7nOE=
+zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615/go.mod h1:8zhuXghYxes7SXEgi6tugjG8iN3XE/M7HdYOTqTnCbI=
 zgo.at/errors v1.4.0 h1:vFMu0mNUMOYtV1jufpZQ8LDvI5/JYTLH8y0NjzZnNeg=
 zgo.at/errors v1.4.0/go.mod h1:POfgvh1LafF2NZJk6buGYCIhcHWuR/miB3nndyf3ozs=
 zgo.at/follow v0.0.0-20250412000721-fecf5127a123 h1:ogO8HrEECBi9igWgza1oUGdezlKCsAQE3UQhLOttiUY=

pkg/email_log/email.go
@@ -69,7 +69,7 @@ func (e Email) Handle(ctx context.Context, r slog.Record) error {
 	subject = strings.TrimLeft(subject, " \t:")
 
 	go func() {
-		err := blackmail.Send(subject,
+		err := blackmail.Get(ctx).Send(subject,
 			blackmail.From("", e.from),
 			blackmail.To(e.to),
 			blackmail.BodyText([]byte(msg)))

cmd/goatcounter/main_test.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"testing"
 
-	"zgo.at/blackmail"
 	"zgo.at/goatcounter/v2"
 	"zgo.at/goatcounter/v2/cron"
 	"zgo.at/goatcounter/v2/gctest"
@@ -31,8 +30,6 @@ func startTest(t *testing.T) (
 	ctx context.Context, dbc string,
 ) {
 	t.Helper()
-
-	blackmail.DefaultMailer = blackmail.NewMailer(blackmail.ConnectWriter)
 
 	goatcounter.Memstore.Reset()
 

context.go
@@ -3,9 +3,11 @@ package goatcounter
 import (
 	"context"
 	"fmt"
+	"os"
 	"runtime/debug"
 	"time"
 
+	"zgo.at/blackmail"
 	"zgo.at/goatcounter/v2/pkg/geo"
 	"zgo.at/z18n"
 	"zgo.at/zcache/v2"
@@ -205,6 +207,9 @@ func CopyContextValues(ctx context.Context) context.Context {
 	if g := geo.Get(ctx); g != nil {
 		n = geo.With(n, g)
 	}
+	if m := blackmail.Get(ctx); m != nil {
+		n = blackmail.With(ctx, m)
+	}
 	return n
 }
 
@@ -214,6 +219,11 @@ func NewContext(ctx context.Context, db zdb.DB) context.Context {
 	n = geo.With(n, geo.Get(ctx))
 	n = NewCache(n)
 	n = NewConfig(n)
+	m := blackmail.Get(ctx)
+	if m != nil {
+		m = blackmail.NewWriter(os.Stderr)
+	}
+	n = blackmail.With(n, m)
 	return n
 }
 

cron/cron.go
@@ -30,7 +30,7 @@ var Tasks = []Task{
 	{"vacuum soft-deleted sites", vacuumDeleted, 12 * time.Hour},
 	{"rm old exports", oldExports, 1 * time.Hour},
 	{"cycle sessions", sessions, 1 * time.Minute},
-	{"send email reports", emailReports, 1 * time.Hour},
+	{"send email reports", EmailReports, 1 * time.Hour},
 	{"persist hits", persistAndStat, time.Duration(persistInterval.Load())},
 }
 

cron/email_reports.go
@@ -21,7 +21,7 @@ import (
 var el = log.Module("email-report")
 
 // EmailReports sends email reports for sites that have this configured.
-func emailReports(ctx context.Context) error {
+func EmailReports(ctx context.Context) error {
 	users, err := reportUsers(ctx)
 	if err != nil {
 		return errors.Errorf("cron.emailReports: %w", err)
@@ -57,7 +57,7 @@ func emailReports(ctx context.Context) error {
 			continue
 		}
 
-		err = blackmail.Send(subject,
+		err = blackmail.Get(ctx).Send(subject,
 			blackmail.From("GoatCounter reports", goatcounter.Config(ctx).EmailFrom),
 			blackmail.To(user.Email),
 			blackmail.HeadersAutoreply(),

commit 3114ab00eb968cd3544748658207688d2372daa4

Fix -errors=mailto
Regression from 973955bc; have to pass the mailer to email_log.New()

Also update blackmail so it won't use quoted-printable when writing
emails to stdout. It's such a pain to read and copy things like URLs out
of there.

cmd/goatcounter/serve.go
@@ -232,8 +232,8 @@ func cmdServe(f zli.Flags, ready chan<- struct{}, stop chan struct{}, saas bool)
 		*flagTLS.Pointer() = map[bool]string{true: "http", false: "acme"}[dev.Bool()]
 	}
 
-	flagErrors(&v, errorsFlag.String())
 	mailer := flagEmail(&v, smtp.String())
+	flagErrors(&v, errorsFlag.String(), mailer)
 	geodb := setupGeo(&v, geodbFlag.String())
 	ratelimits := setupRatelimits(&v, ratelimit.String())
 	*from.Pointer() = flagFrom(&v, saas, from.String(), domain.String())
@@ -452,7 +452,7 @@ func setupReload() error {
 	return nil
 }
 
-func flagErrors(v *zvalidate.Validator, errors string) {
+func flagErrors(v *zvalidate.Validator, errors string, mailer blackmail.Mailer) {
 	switch {
 	default:
 		v.Append("-errors", "invalid value")
@@ -468,7 +468,7 @@ func flagErrors(v *zvalidate.Validator, errors string) {
 		v.Email("-errors", to)
 		slog.SetDefault(slog.New(log.NewChain(
 			slog.Default().Handler(),
-			email_log.New(slog.LevelWarn, from, to),
+			email_log.New(mailer, slog.LevelWarn, from, to),
 		)))
 	}
 }

go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/BurntSushi/toml v1.5.0
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/teamwork/reload v1.4.2
-	zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615
+	zgo.at/blackmail v0.0.0-20251210171630-f1b7a42e3f87
 	zgo.at/errors v1.4.0
 	zgo.at/follow v0.0.0-20250412000721-fecf5127a123
 	zgo.at/gadget v1.0.0

go.sum
@@ -57,8 +57,8 @@ golang.org/x/text v0.31.0 h1:aC8ghyu4JhP8VojJ2lEHBnochRno1sgL6nEi9WGFGMM=
 golang.org/x/text v0.31.0/go.mod h1:tKRAlv61yKIjGGHX/4tP1LTbc13YSec1pxVEWXzfoeM=
 golang.org/x/tools v0.38.0 h1:Hx2Xv8hISq8Lm16jvBZ2VQf+RLmbd7wVUsALibYI/IQ=
 golang.org/x/tools v0.38.0/go.mod h1:yEsQ/d/YK8cjh0L6rZlY8tgtlKiBNTL14pGDJPJpYQs=
-zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615 h1:AdTk8a17k7bJv2OIGnN6JHtqljSVltt0PG+jcLG7nOE=
-zgo.at/blackmail v0.0.0-20251205074349-835f88ad0615/go.mod h1:8zhuXghYxes7SXEgi6tugjG8iN3XE/M7HdYOTqTnCbI=
+zgo.at/blackmail v0.0.0-20251210171630-f1b7a42e3f87 h1:O61inXZr1VOLavOsgwLs6Nci5YdPxF7rEAYrH/n2x/Y=
+zgo.at/blackmail v0.0.0-20251210171630-f1b7a42e3f87/go.mod h1:8zhuXghYxes7SXEgi6tugjG8iN3XE/M7HdYOTqTnCbI=
 zgo.at/errors v1.4.0 h1:vFMu0mNUMOYtV1jufpZQ8LDvI5/JYTLH8y0NjzZnNeg=
 zgo.at/errors v1.4.0/go.mod h1:POfgvh1LafF2NZJk6buGYCIhcHWuR/miB3nndyf3ozs=
 zgo.at/follow v0.0.0-20250412000721-fecf5127a123 h1:ogO8HrEECBi9igWgza1oUGdezlKCsAQE3UQhLOttiUY=

pkg/email_log/email.go
@@ -14,12 +14,13 @@ import (
 )
 
 type Email struct {
+	m        blackmail.Mailer
 	from, to string
 	lvl      slog.Level
 }
 
-func New(lvl slog.Level, from, to string) Email {
-	return Email{from: from, to: to, lvl: lvl}
+func New(mailer blackmail.Mailer, lvl slog.Level, from, to string) Email {
+	return Email{m: mailer, from: from, to: to, lvl: lvl}
 }
 
 func (e Email) Enabled(ctx context.Context, l slog.Level) bool {
@@ -69,7 +70,7 @@ func (e Email) Handle(ctx context.Context, r slog.Record) error {
 	subject = strings.TrimLeft(subject, " \t:")
 
 	go func() {
-		err := blackmail.Get(ctx).Send(subject,
+		err := e.m.Send(subject,
 			blackmail.From("", e.from),
 			blackmail.To(e.to),
 			blackmail.BodyText([]byte(msg)))