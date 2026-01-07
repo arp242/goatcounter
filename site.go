arp242/goatcounter

commit ac08b28dd7d05d804e6a7ec7b8e9f034accd973a

Use zdb.{Insert,Update}

site.go
@@ -13,6 +13,7 @@ import (
 	"zgo.at/guru"
 	"zgo.at/json"
 	"zgo.at/zdb"
+	"zgo.at/zstd/zbool"
 	"zgo.at/zstd/zcrypto"
 	"zgo.at/zstd/znet"
 	"zgo.at/zstd/zslice"
@@ -98,8 +99,8 @@ func (s SiteIDs) List(ctx context.Context) Sites {
 }
 
 type Site struct {
-	ID     SiteID  `db:"site_id" json:"id,readonly"`
-	Parent *SiteID `db:"parent" json:"parent,readonly"`
+	ID     SiteID  `db:"site_id,id" json:"id,readonly"`
+	Parent *SiteID `db:"parent,readonly" json:"parent,readonly"`
 
 	// Custom domain, e.g. "stats.example.com".
 	//
@@ -111,7 +112,7 @@ type Site struct {
 
 	// Domain code (e.g. "arp242", which makes arp242.goatcounter.com). Only
 	// used for goatcounter.com and not when self-hosting.
-	Code string `db:"code" json:"code"`
+	Code string `db:"code,readonly" json:"code"`
 
 	// Site domain for linking (www.arp242.net). Note this can be a full URL and
 	// is a bit misnamed.
@@ -122,16 +123,15 @@ type Site struct {
 
 	// Whether this site has received any data; will be true after the first
 	// pageview.
-	ReceivedData bool `db:"received_data" json:"received_data"`
-
-	// {omitdoc}
-	Notes string `db:"notes" json:"-"`
+	ReceivedData zbool.Bool `db:"received_data" json:"received_data"`
 
 	State      string     `db:"state" json:"state"`
-	CreatedAt  time.Time  `db:"created_at" json:"created_at"`
+	CreatedAt  time.Time  `db:"created_at,readonly" json:"created_at"`
 	UpdatedAt  *time.Time `db:"updated_at" json:"updated_at"`
-	FirstHitAt time.Time  `db:"first_hit_at" json:"first_hit_at"`
+	FirstHitAt time.Time  `db:"first_hit_at,readonly" json:"first_hit_at"`
 }
+
+func (Site) Table() string { return "sites" }
 
 // ClearCache clears the cache for this site.
 func (s Site) ClearCache(ctx context.Context, full bool) {
@@ -144,7 +144,8 @@ func (s Site) ClearCache(ctx context.Context, full bool) {
 	}
 }
 
-// Defaults sets fields to default values, unless they're already set.
+var _ zdb.Defaulter = &Site{}
+
 func (s *Site) Defaults(ctx context.Context) {
 	if s.State == "" {
 		s.State = StateActive
@@ -175,7 +176,8 @@ func (s *Site) Defaults(ctx context.Context) {
 
 var noUnderscore = time.Date(2020, 03, 20, 0, 0, 0, 0, time.UTC)
 
-// Validate the object.
+var _ zdb.Validator = &Site{}
+
 func (s *Site) Validate(ctx context.Context) error {
 	v := NewValidate(ctx)
 
@@ -241,44 +243,20 @@ func (s *Site) Validate(ctx context.Context) error {
 	return v.ErrorOrNil()
 }
 
-// Insert a new row.
 func (s *Site) Insert(ctx context.Context) error {
-	if s.ID > 0 {
-		return errors.New("ID > 0")
-	}
-	s.Defaults(ctx)
-	err := s.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
-	s.ID, err = zdb.InsertID[SiteID](ctx, "site_id", `insert into sites (
-		parent, code, cname, link_domain, settings, user_defaults, created_at, first_hit_at, cname_setup_at) values (?)`,
-		[]any{s.Parent, s.Code, s.Cname, s.LinkDomain, s.Settings, s.UserDefaults, s.CreatedAt, s.CreatedAt, s.CnameSetupAt})
-	if err != nil && zdb.ErrUnique(err) {
+	err := zdb.Insert(ctx, s)
+	if zdb.ErrUnique(err) {
 		return guru.New(400, "this site already exists: code or domain must be unique")
 	}
 	return errors.Wrap(err, "Site.Insert")
 }
 
-// Update existing site. Sets settings, cname, link_domain.
 func (s *Site) Update(ctx context.Context) error {
-	if s.ID == 0 {
-		return errors.New("ID == 0")
-	}
-	s.Defaults(ctx)
-	err := s.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
 	if s.Cname == nil || *s.Cname == "" {
 		s.Cname, s.CnameSetupAt = nil, nil
 	}
 
-	err = zdb.Exec(ctx,
-		`update sites set settings=?, user_defaults=?, cname=?, link_domain=?, updated_at=?, cname_setup_at=? where site_id=?`,
-		s.Settings, s.UserDefaults, s.Cname, s.LinkDomain, s.UpdatedAt, s.CnameSetupAt, s.ID)
+	err := zdb.Update(ctx, s, zdb.UpdateAll)
 	if err != nil {
 		return errors.Wrap(err, "Site.Update")
 	}
@@ -315,9 +293,7 @@ func (s *Site) UpdateParent(ctx context.Context, newParent *SiteID) error {
 			}
 		}
 
-		return zdb.Exec(ctx,
-			`update sites set parent=?, updated_at=? where site_id=?`,
-			s.Parent, s.UpdatedAt, s.ID)
+		return zdb.Update(ctx, s, "parent", "updated_at")
 	})
 	if err != nil {
 		return errors.Wrap(err, "Site.UpdateParent")
@@ -330,62 +306,34 @@ func (s *Site) UpdateParent(ctx context.Context, newParent *SiteID) error {
 // UpdateCode changes the site's domain code (e.g. "test" in
 // "test.goatcounter.com").
 func (s *Site) UpdateCode(ctx context.Context, code string) error {
-	if s.ID == 0 {
-		return errors.New("ID == 0")
-	}
-
 	s.Code = code
-
-	s.Defaults(ctx)
-	err := s.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
-	err = zdb.Exec(ctx,
-		`update sites set code=$1, updated_at=$2 where site_id=$3`,
-		s.Code, s.UpdatedAt, s.ID)
+	err := zdb.Update(ctx, s, "code", "updated_at")
 	if err != nil {
 		return errors.Wrap(err, "Site.UpdateCode")
 	}
-
 	cacheSites(ctx).Delete(s.ID)
 	cacheSitesHost(ctx).Reset()
 	return nil
 }
 
 func (s *Site) UpdateReceivedData(ctx context.Context) error {
-	err := zdb.Exec(ctx, `update sites set received_data=1 where site_id=$1`, s.ID)
-
+	err := zdb.Update(ctx, s, "received_data")
 	s.ClearCache(ctx, false)
 	return errors.Wrap(err, "Site.UpdateReceivedData")
 }
 
 func (s *Site) UpdateFirstHitAt(ctx context.Context, f time.Time) error {
-	f = f.UTC().Add(-12 * time.Hour)
-	s.FirstHitAt = f
-	err := zdb.Exec(ctx,
-		`update sites set first_hit_at=$1 where site_id=$2`,
-		s.FirstHitAt, s.ID)
-
+	s.FirstHitAt = f.UTC().Add(-12 * time.Hour)
+	err := zdb.Update(ctx, s, "first_hit_at")
 	s.ClearCache(ctx, false)
 	return errors.Wrap(err, "Site.UpdateFirstHitAt")
 }
 
 // UpdateCnameSetupAt confirms the custom domain was setup correct.
 func (s *Site) UpdateCnameSetupAt(ctx context.Context) error {
-	if s.ID == 0 {
-		return errors.New("ID == 0")
-	}
-
-	err := zdb.Exec(ctx,
-		`update sites set cname_setup_at=$1 where site_id=$2`,
-		s.CnameSetupAt, s.ID)
-	if err != nil {
-		return errors.Wrap(err, "Site.UpdateCnameSetupAt")
-	}
+	err := zdb.Update(ctx, s, "cname_setup_at")
 	s.ClearCache(ctx, false)
-	return nil
+	return errors.Wrap(err, "Site.UpdateCnameSetupAt")
 }
 
 // Delete a site and all child sites.
@@ -452,7 +400,8 @@ func (s Site) Undelete(ctx context.Context, id SiteID) error {
 	}
 
 	s.ClearCache(ctx, false)
-	return errors.Wrap(s.ByID(ctx, id), "Site.Undelete")
+	err = s.ByID(ctx, id)
+	return errors.Wrap(err, "Site.Undelete")
 }
 
 // Exists checks if this site already exists, based on either the Cname or Code
@@ -720,24 +669,25 @@ type Sites []Site
 
 // UnscopedList lists all sites, not scoped to the current user.
 func (s *Sites) UnscopedList(ctx context.Context) error {
-	return errors.Wrap(zdb.Select(ctx, s,
-		`/* Sites.List */ select * from sites where state=$1`,
-		StateActive), "Sites.List")
+	err := zdb.Select(ctx, s, `/* Sites.List */ select * from sites where state=$1`, StateActive)
+	return errors.Wrap(err, "Sites.List")
 }
 
 // UnscopedListCnames all sites that have CNAME set, not scoped to the current
 // user.
 func (s *Sites) UnscopedListCnames(ctx context.Context) error {
-	return errors.Wrap(zdb.Select(ctx, s, `/* Sites.ListCnames */
+	err := zdb.Select(ctx, s, `/* Sites.ListCnames */
 		select * from sites where state=$1 and cname is not null`,
-		StateActive), "Sites.List")
+		StateActive)
+	return errors.Wrap(err, "Sites.List")
 }
 
 // ListSubs lists all subsites for the current site.
 func (s *Sites) ListSubs(ctx context.Context) error {
-	return errors.Wrap(zdb.Select(ctx, s, `/* Sites.ListSubs */
+	err := zdb.Select(ctx, s, `/* Sites.ListSubs */
 		select * from sites where parent=$1 and state=$2 order by code`,
-		MustGetSite(ctx).ID, StateActive), "Sites.ListSubs")
+		MustGetSite(ctx).ID, StateActive)
+	return errors.Wrap(err, "Sites.ListSubs")
 }
 
 // ForAccount gets all sites associated with an account.

api_token.go
@@ -32,7 +32,7 @@ const (
 type APITokenID int32
 
 type APIToken struct {
-	ID     APITokenID `db:"api_token_id" json:"-"`
+	ID     APITokenID `db:"api_token_id,id" json:"-"`
 	SiteID SiteID     `db:"site_id" json:"-"`
 	UserID UserID     `db:"user_id" json:"-"`
 
@@ -44,6 +44,8 @@ type APIToken struct {
 	CreatedAt  time.Time  `db:"created_at" json:"-"`
 	LastUsedAt *time.Time `db:"last_used_at" json:"-"`
 }
+
+func (APIToken) Table() string { return "api_tokens" }
 
 type PermissionFlag struct {
 	Label, Help string
@@ -125,12 +127,16 @@ func (t APIToken) FormatPermissions() string {
 	return "'" + strings.Join(all, "', '") + "'"
 }
 
-// Defaults sets fields to default values, unless they're already set.
+var _ zdb.Defaulter = &APIToken{}
+
 func (t *APIToken) Defaults(ctx context.Context) {
 	t.SiteID = MustGetSite(ctx).IDOrParent()
+	t.UserID = MustGetUser(ctx).ID
 	t.Token = zcrypto.Secret256()
 	t.CreatedAt = ztime.Now(ctx)
 }
+
+var _ zdb.Validator = &APIToken{}
 
 func (t *APIToken) Validate(ctx context.Context) error {
 	v := NewValidate(ctx)
@@ -162,55 +168,21 @@ func (t *APIToken) Validate(ctx context.Context) error {
 
 // Insert a new row.
 func (t *APIToken) Insert(ctx context.Context) error {
-	if t.ID > 0 {
-		return errors.New("ID > 0")
-	}
-
-	t.Defaults(ctx)
-	err := t.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
-	t.ID, err = zdb.InsertID[APITokenID](ctx, "api_token_id",
-		`insert into api_tokens (site_id, user_id, name, token, permissions, created_at, sites) values (?)`,
-		[]any{t.SiteID, GetUser(ctx).ID, t.Name, t.Token, t.Permissions, t.CreatedAt, t.Sites})
+	err := zdb.Insert(ctx, t)
 	return errors.Wrap(err, "APIToken.Insert")
 }
 
 // Update the name and permissions.
 func (t *APIToken) Update(ctx context.Context) error {
-	if t.ID == 0 {
-		return errors.New("ID == 0")
-	}
-
-	t.Defaults(ctx)
-	err := t.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
-	err = zdb.Exec(ctx, `update api_tokens set name=?, permissions=? where api_token_id=?`,
-		t.Name, t.Permissions, t.ID)
+	err := zdb.Update(ctx, t, "name", "permissions")
 	return errors.Wrap(err, "APIToken.Update")
 }
 
-// UpdateLastUsed sets the last used time to the current time.
-func (t *APIToken) UpdateLastUsed(ctx context.Context) error {
-	if t.ID == 0 {
-		return errors.New("ID == 0")
-	}
-
-	t.Defaults(ctx)
-	err := t.Validate(ctx)
-	if err != nil {
-		return err
-	}
-
+// Touch sets the last used time to the current time.
+func (t *APIToken) Touch(ctx context.Context) error {
 	t.LastUsedAt = ztype.Ptr(ztime.Now(ctx))
-	err = zdb.Exec(ctx, `update api_tokens set last_used_at=? where api_token_id=?`,
-		t.LastUsedAt, t.ID)
-	return errors.Wrap(err, "APIToken.UpdateLastUsed")
+	err := zdb.Update(ctx, t, "last_used_at")
+	return errors.Wrap(err, "APIToken.Touch")
 }
 
 func (t *APIToken) ByID(ctx context.Context, id APITokenID) error {
@@ -222,7 +194,7 @@ func (t *APIToken) ByID(ctx context.Context, id APITokenID) error {
 
 func (t *APIToken) ByToken(ctx context.Context, token string) error {
 	err := zdb.Get(ctx, t,
-		`/* APIToken.ByID */ select * from api_tokens where token=$1 and site_id=$2`,
+		`/* APIToken.ByToken */ select * from api_tokens where token=$1 and site_id=$2`,
 		token, MustGetSite(ctx).IDOrParent())
 	if err != nil {
 		return errors.Wrapf(err, "APIToken.ByToken(%q)", token)

campaign.go
@@ -11,36 +11,33 @@ import (
 type CampaignID int32
 
 type Campaign struct {
-	ID     CampaignID `db:"campaign_id" json:"campaign_id"`
+	ID     CampaignID `db:"campaign_id,id" json:"campaign_id"`
 	SiteID SiteID     `db:"site_id" json:"site_id"`
 	Name   string     `db:"name" json:"name"`
 }
 
-func (c *Campaign) Defaults(ctx context.Context) {}
+func (Campaign) Table() string { return "campaigns" }
 
-func (c *Campaign) Validate() error {
+var _ zdb.Defaulter = &Campaign{}
+
+func (c *Campaign) Defaults(ctx context.Context) {
+	if c.SiteID == 0 {
+		c.SiteID = MustGetSite(ctx).ID
+	}
+}
+
+var _ zdb.Validator = &Campaign{}
+
+func (c *Campaign) Validate(ctx context.Context) error {
 	v := zvalidate.New()
 	v.Required("name", c.Name)
+	v.Required("site_id", c.SiteID)
 	return v.ErrorOrNil()
 }
 
 func (c *Campaign) Insert(ctx context.Context) error {
-	if c.ID > 0 {
-		return errors.Errorf("Campaign.Insert: c.ID>0: %d", c.ID)
-	}
-
-	c.Defaults(ctx)
-	err := c.Validate()
-	if err != nil {
-		return errors.Wrap(err, "Campaign.Insert")
-	}
-
-	c.ID, err = zdb.InsertID[CampaignID](ctx, "campaign_id",
-		`insert into campaigns (site_id, name) values (?, ?)`, MustGetSite(ctx).ID, c.Name)
-	if err != nil {
-		return errors.Wrap(err, "Campaign.Insert")
-	}
-	return nil
+	err := zdb.Insert(ctx, c)
+	return errors.Wrap(err, "Campaign.Insert")
 }
 
 func (c *Campaign) ByName(ctx context.Context, name string) error {

db/migrate/gomig/2022-11-15-1-correct-hit-stats.go
@@ -60,7 +60,7 @@ func CorrectHitStats(ctx context.Context) error {
 var empty = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 func updateHitStats(ctx context.Context, hits []Hit) error {
-	return errors.Wrap(zdb.TX(ctx, func(ctx context.Context) error {
+	err := zdb.TX(ctx, func(ctx context.Context) error {
 		type gt struct {
 			count  []int
 			day    string
@@ -128,7 +128,8 @@ func updateHitStats(ctx context.Context, hits []Hit) error {
 			ins.Values(siteID, v.day, v.pathID, zjson.MustMarshal(v.count))
 		}
 		return errors.Wrap(ins.Finish(), "updateHitStats hit_stats")
-	}), "cron.updateHitStats")
+	})
+	return errors.Wrap(err, "cron.updateHitStats")
 }
 
 func existingHitStats(ctx context.Context, siteID int64, day string, pathID int64) ([]int, error) {

export.go
@@ -7,12 +7,13 @@ import (
 
 	"zgo.at/errors"
 	"zgo.at/zdb"
+	"zgo.at/zvalidate"
 )
 
 type ExportID int32
 
 type Export struct {
-	ID     ExportID `db:"export_id" json:"id,readonly"`
+	ID     ExportID `db:"export_id,id" json:"id,readonly"`
 	SiteID SiteID   `db:"site_id" json:"site_id,readonly"`
 
 	// The hit ID this export was started from.
@@ -35,6 +36,24 @@ type Export struct {
 
 	// Any errors that may have occured.
 	Error *string `db:"error" json:"error,readonly"`
+}
+
+func (Export) Table() string { return "exports" }
+
+var _ zdb.Defaulter = &Export{}
+
+func (e *Export) Defaults(ctx context.Context) {
+	if e.SiteID == 0 {
+		e.SiteID = MustGetSite(ctx).ID
+	}
+}
+
+var _ zdb.Validator = &Export{}
+
+func (e *Export) Validate(ctx context.Context) error {
+	v := zvalidate.New()
+	v.Required("site_id", e.SiteID)
+	return v.ErrorOrNil()
 }
 
 func (e *Export) ByID(ctx context.Context, id ExportID) error {

export_test.go
@@ -1,1 +1,0 @@
-package goatcounter_test

exportcsv.go
@@ -19,6 +19,7 @@ import (
 	"zgo.at/zstd/zcrypto"
 	"zgo.at/zstd/zint"
 	"zgo.at/zstd/ztime"
+	"zgo.at/zstd/ztype"
 )
 
 const ExportCSVVersion = "2"
@@ -37,10 +38,7 @@ func (e *Export) CreateCSV(ctx context.Context, startFrom HitID) (*os.File, erro
 		os.TempDir(), string(os.PathSeparator), site.Code,
 		e.CreatedAt.Format("20060102T150405Z"), startFrom)
 
-	var err error
-	e.ID, err = zdb.InsertID[ExportID](ctx, "export_id",
-		`insert into exports (site_id, path, created_at, start_from_hit_id) values (?, ?, ?, ?)`,
-		e.SiteID, e.Path, e.CreatedAt, e.StartFromHitID)
+	err := zdb.Insert(ctx, e)
 	if err != nil {
 		return nil, errors.Wrap(err, "Export.Create")
 	}
@@ -104,9 +102,8 @@ func (e *Export) RunCSV(ctx context.Context, fp *os.File, mailUser bool) {
 
 	if exportErr != nil {
 		l.Error(ctx, exportErr, "export", e)
-		err := zdb.Exec(ctx,
-			`update exports set error=$1 where export_id=$2`,
-			exportErr.Error(), e.ID)
+		e.Error = ztype.Ptr(exportErr.Error())
+		err := zdb.Update(ctx, e, "error")
 		if err != nil {
 			log.Error(ctx, err)
 		}
@@ -151,11 +148,8 @@ func (e *Export) RunCSV(ctx context.Context, fp *os.File, mailUser bool) {
 		return
 	}
 
-	now := ztime.Now(ctx)
-	err = zdb.Exec(ctx, `update exports set
-		finished_at=$1, num_rows=$2, size=$3, hash=$4, last_hit_id=$5
-		where export_id=$6`,
-		&now, e.NumRows, e.Size, e.Hash, e.LastHitID, e.ID)
+	e.FinishedAt = ztype.Ptr(ztime.Now(ctx))
+	zdb.Update(ctx, e, "finished_at", "num_rows", "size", "hash", "last_hit_id")
 	if err != nil {
 		log.Error(ctx, err)
 	}

exportcsv_test.go
@@ -92,7 +92,7 @@ func TestCSVExport(t *testing.T) {
 			"last_hit_id": 4,
 			"path": "%(ANY)goatcounter-export-gctest2-%(YEAR)%(MONTH)%(DAY)T%(ANY)Z-0.csv.gz",
 			"created_at": "%(YEAR)-%(MONTH)-%(DAY)T%(ANY)Z",
-			"finished_at": null,
+			"finished_at": "%(YEAR)-%(MONTH)-%(DAY)T%(ANY)Z",
 			"num_rows": 4,
 			"size": "0.1",
 			"hash": "sha256-8a426c2bebfa343e2dc20e154b67297e981ac3399202369485d6ede82979205b",

commit 674afa8c9f656a555ae989f6ed89aa8d76b13c24

Fix setting received_data after receiving the first pageview
Stupid regression from ac08b28d

site.go
@@ -317,6 +317,7 @@ func (s *Site) UpdateCode(ctx context.Context, code string) error {
 }
 
 func (s *Site) UpdateReceivedData(ctx context.Context) error {
+	s.ReceivedData = true
 	err := zdb.Update(ctx, s, "received_data")
 	s.ClearCache(ctx, false)
 	return errors.Wrap(err, "Site.UpdateReceivedData")