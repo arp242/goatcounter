arp242/goatcounter

commit 9903e20a14297615a3bcb27c25669a572ca5d201

Remove "Current [week] · [month] · [year]" buttons
I've long since somewhat disliked the "Current [week] · [month] ·
[year]" buttons; they never seemed all that useful and it kind of
clutters the top bar. The original idea was that it's easy(ish) to
compare time periods by selecting the entire current period and then
going back. i.e. this year vs. last year. But I don't think many people
are really using it as such. And it's easy enough to manually fill in
the dates. And you have the +/- diff % numbers now.

All of that said, I never removed it because some people do use it; I
added some metrics for it last year in fd09952e, and some people click
that button. Although just a few a day, and I can't know if they
actually want to click it, click it by accident, or click it just to see
what it does (new people sign up every day).

There are also 19 people who have year-cur, month-cur, or week-cur as
the default view, which seems like a very odd default because if it's
near the start of the period, the stats are pretty useless? The current
year shows very little as it's Jan 8. Even the current month doesn't
show all that much.

But now its kind of in the way of #865, and would keeping it complicate
that quite a bit. So just remove it. And with it, remove the ability to
enter future dates that are shown grayed out.

Do make sure the X-cur views still work by dropping the -cur, so
week-cur becomes just week.

handlers/dashboard.go
@@ -44,6 +44,7 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 
 	// Load view, but override this from query.
 	view, _ := user.Settings.Views.Get("default")
+	view.Period = strings.TrimSuffix(view.Period, "-cur")
 
 	rng, err := getPeriod(w, r, site, user)
 	if err != nil {
@@ -55,11 +56,7 @@ func (h backend) dashboard(w http.ResponseWriter, r *http.Request) error {
 			return err
 		}
 	} else {
-		view.Period = q.Get("hl-period")
-	}
-	// Record how often people use the "Current [..]" buttons
-	if strings.HasSuffix(view.Period, "-cur") {
-		metrics.Start(view.Period).Done()
+		view.Period = strings.TrimSuffix(q.Get("hl-period"), "-cur")
 	}
 
 	showRefs, _ := zstrconv.ParseInt[goatcounter.PathID](q.Get("showrefs"), 10)
@@ -358,21 +355,12 @@ func (h backend) loadWidget(w http.ResponseWriter, r *http.Request) error {
 //	   The start date is set to exactly this period ago. The end date is set to
 //	   the end of the current day.
 //
-//	week-cur, month-cur, year-cur
-//	   The current week or month; both the start and return are modified.
-//
 //	Any digit
 //	   Last n days.
 func timeRange(ctx context.Context, r string, tz *time.Location, sundayStartsWeek bool) ztime.Range {
 	rng := ztime.NewRange(ztime.Now(ctx).In(tz)).Current(ztime.Day)
 	switch r {
 	case "0", "day":
-	case "week-cur":
-		rng = rng.Current(ztime.Week(sundayStartsWeek))
-	case "month-cur":
-		rng = rng.Current(ztime.Month)
-	case "year-cur":
-		rng = rng.Current(ztime.Year)
 	case "week":
 		rng = rng.Last(ztime.Week(sundayStartsWeek))
 	case "month":
@@ -420,6 +408,10 @@ func getPeriod(w http.ResponseWriter, r *http.Request, site *goatcounter.Site, u
 	if rng.Start.Before(c) {
 		y, m, d := c.In(user.Settings.Timezone.Loc()).Date()
 		rng.Start = time.Date(y, m, d, 0, 0, 0, 0, user.Settings.Timezone.Loc())
+	}
+	n := ztime.EndOf(ztime.Now(r.Context()), ztime.Hour)
+	if rng.End.After(n) {
+		rng.End = n
 	}
 
 	return rng.From(rng.Start).To(rng.End).UTC(), nil

widgets/pages.go
@@ -121,28 +121,6 @@ func (w Pages) RenderHTML(ctx context.Context, shared SharedData) (string, any) 
 	}
 	t += ".gohtml"
 
-	// Correct max for chunked data in text view.
-	if w.Style != "text" && len(w.Pages) > 0 && len(w.Pages[0].Stats) > 0 {
-		// Set days in the future to -1; we filter this in the JS when rendering
-		// the chart.
-		// It's easier to do this here because JavaScript Date() has piss-poor
-		// support for timezones.
-		//
-		// Only remove them if the last day is today: for everything else we
-		// want to display the future as "greyed out".
-		var (
-			now   = ztime.Now(ctx).In(goatcounter.MustGetUser(ctx).Settings.Timezone.Loc())
-			today = now.Format("2006-01-02")
-			hour  = now.Hour()
-		)
-		if w.Pages[0].Stats[len(w.Pages[0].Stats)-1].Day == today {
-			for i := range w.Pages {
-				j := len(w.Pages[i].Stats) - 1
-				w.Pages[i].Stats[j].Hourly = w.Pages[i].Stats[j].Hourly[:hour+1]
-			}
-		}
-	}
-
 	return t, struct {
 		Context context.Context
 		Site    *goatcounter.Site

widgets/total_pages.go
@@ -6,7 +6,6 @@ import (
 
 	"zgo.at/goatcounter/v2"
 	"zgo.at/z18n"
-	"zgo.at/zstd/ztime"
 )
 
 type TotalPages struct {
@@ -54,23 +53,6 @@ func (w *TotalPages) GetData(ctx context.Context, a Args) (more bool, err error)
 }
 
 func (w TotalPages) RenderHTML(ctx context.Context, shared SharedData) (string, any) {
-	// Set days in the future to -1; we filter this in the JS when rendering
-	// the chart.
-	// It's easier to do this here because JavaScript Date() has piss-poor
-	// support for timezones.
-	//
-	// Only remove them if the last day is today: for everything else we
-	// want to display the future as "greyed out".
-	var (
-		now   = ztime.Now(ctx).In(goatcounter.MustGetUser(ctx).Settings.Timezone.Loc())
-		today = now.Format("2006-01-02")
-		hour  = now.Hour()
-	)
-	if len(w.Total.Stats) > 0 && w.Total.Stats[len(w.Total.Stats)-1].Day == today {
-		j := len(w.Total.Stats) - 1
-		w.Total.Stats[j].Hourly = w.Total.Stats[j].Hourly[:hour+1]
-	}
-
 	return "_dashboard_totals.gohtml", struct {
 		Context context.Context
 		Site    *goatcounter.Site

handlers/dashboard_test.go
@@ -39,15 +39,6 @@ func TestTimeRange(t *testing.T) {
 		{"year", "2020-01-18",
 			"2019-01-18 00:00:00", "2020-01-18 23:59:59"},
 
-		// TODO: also test with sundayStartsWeek
-		{"week-cur", "2020-01-01",
-			"2019-12-30 00:00:00", "2020-01-05 23:59:59"},
-
-		{"month-cur", "2020-01-01",
-			"2020-01-01 00:00:00", "2020-01-31 23:59:59"},
-		{"month-cur", "2020-01-31",
-			"2020-01-01 00:00:00", "2020-01-31 23:59:59"},
-
 		{"0", "2020-06-18",
 			"2020-06-18 00:00:00", "2020-06-18 23:59:59"},
 		{"1", "2020-06-18",

handlers/handlers.go
@@ -121,7 +121,6 @@ func newGlobals(w http.ResponseWriter, r *http.Request) Globals {
 			"error/date-mismatch":         T(ctx, "error/date-mismatch|end date is before start date"),
 			"error/load-url":              T(ctx, "error/load-url|Could not load %(url): %(error)", z18n.P{"url": "%(url)", "error": "%(error)"}),
 			"notify/saved":                T(ctx, "notify/saved|Saved!"),
-			"dashboard/future":            T(ctx, "dashboard/future|future"),
 			"dashboard/tooltip-event":     T(ctx, "dashboard/tooltip-event|%(unique) clicks; %(clicks) total clicks", z18n.P{"unique": "%(unique)", "clicks": "%(clicks)"}),
 			"dashboard/totals/num-visits": T(ctx, "dashboard/totals/num-visits|%(num-visits) visits", z18n.P{"num-visits": "%(num-visits)"}),
 			"datepicker/keyboard":         T(ctx, "datepicker/keyboard|Use the arrow keys to pick a date"),

public/backend.css
@@ -288,9 +288,6 @@ tr.target .chart-left { display: block; }
 .period-quarter [value=quarter],
 .period-half-year [value=half-year],
 .period-year [value=year],
-.period-week-cur [value=week-cur],
-.period-month-cur [value=month-cur],
-.period-year-cur [value=year-cur],
 .group-hour [value=hour],
 .group-day [value=day],
 .group-week [value=week] {

public/dashboard.js
@@ -232,21 +232,6 @@
 				case 'quarter':   start.setMonth(start.getMonth() - 3); break;
 				case 'half-year': start.setMonth(start.getMonth() - 6); break;
 				case 'year':      start.setFullYear(start.getFullYear() - 1); break;
-				case 'week-cur':
-					if (USER_SETTINGS.sunday_starts_week)
-						start.setDate(start.getDate() - start.getDay())
-					else
-						start.setDate(start.getDate() - start.getDay() + (start.getDay() ? 1 : -6))
-					end = new Date(start.getFullYear(), start.getMonth(), start.getDate() + 6)
-					break;
-				case 'month-cur':
-					start.setDate(1)
-					end = new Date(end.getFullYear(), end.getMonth() + 1, 0)
-					break
-				case 'year-cur':
-					start = new Date(start.getFullYear(), 0, 1)
-					end = new Date(end.getFullYear(), 12, 0)
-					break
 			}
 
 			$('#hl-period').val(this.value).attr('disabled', false)
@@ -601,7 +586,6 @@
 		else
 			data = stats.map((s) => s.hourly).reduce((a, b) => a.concat(b))
 
-		let futureFrom = 0
 		var chart = charty(ctx, data, {
 			mode: isBar ? 'bar' : 'line',
 			max:  max,
@@ -611,20 +595,6 @@
 				width: daily || ndays <= 14 ? 1.5 : 1
 			},
 			bar:  {color: style('chart-line')},
-			done: (chart) => {
-				// Show future as greyed out.
-				let last   = stats[stats.length - 1].day + (daily ? '' : ' 23:59:59'),
-					future = last > format_date_ymd(new Date()) + (daily ? '' : ' 23:59:59')
-				if (future) {
-					let dpr   = Math.max(1, window.devicePixelRatio || 1),
-						width = chart.barWidth() * ((get_date(last) - new Date()) / ((daily ? 86400 : 3600) * 1000))
-					futureFrom = canvas.width/dpr - width - chart.pad()
-
-					ctx.fillStyle = '#ddd'
-					ctx.beginPath()
-					ctx.fillRect(futureFrom, (chart.pad()-1), width, canvas.height/dpr - chart.pad()*2 + 2)
-				}
-			},
 		})
 		charts.push(chart)
 
@@ -648,15 +618,12 @@
 				visits = daily ? day.daily : day.hourly[i%24],
 				views  = daily ? day.daily : day.hourly[i%24]
 
-			let title = '',
-				future = futureFrom && x >= futureFrom - 1
+			let title = ''
 			if (daily)
 				title = `${format_date(day.day, true)}`
 			else
 				title = `${format_date(day.day, true)} ${un24(start)} – ${un24(end)}`
-			if (future)
-				title += '; ' + T('dashboard/future')
-			if (!future && !USER_SETTINGS.fewer_numbers) {
+			if (!USER_SETTINGS.fewer_numbers) {
 				if (isEvent) {
 					title += '; ' + T('dashboard/tooltip-event', {
 						unique: format_int(visits),

settings.go
@@ -112,7 +112,7 @@ type (
 		Name   string `json:"name"`
 		Filter string `json:"filter"`
 		Group  Group  `json:"group"`
-		Period string `json:"period"` // "week", "week-cur", or n days: "8"
+		Period string `json:"period"` // "week", "month", etc., or n days: "8"
 	}
 	Views []View
 )

commit dab28d576ca15435c20620d71afdd937e0b9b8f2

Fix viewing a single day
Broke with 9903e20a. Fixing the end date here doesn't really work, and
the bit about setting future dates to -1 is needed. Need to look at this
further – viewing a single day doesn't really work all that great in the
first place because it will show you from "08 jan to 08 jan", which
could just be an hour or two.

Need to think about what to do about this. But for now this at least
fixes JS erroring out and not rendering any charts at all.

handlers/dashboard.go
@@ -409,10 +409,6 @@ func getPeriod(w http.ResponseWriter, r *http.Request, site *goatcounter.Site, u
 		y, m, d := c.In(user.Settings.Timezone.Loc()).Date()
 		rng.Start = time.Date(y, m, d, 0, 0, 0, 0, user.Settings.Timezone.Loc())
 	}
-	n := ztime.EndOf(ztime.Now(r.Context()), ztime.Hour)
-	if rng.End.After(n) {
-		rng.End = n
-	}
 
 	return rng.From(rng.Start).To(rng.End).UTC(), nil
 }

public/backend.js
@@ -9,6 +9,7 @@
 		window.TZ_OFFSET         = parseInt($('#js-settings').attr('data-offset'), 10) || 0
 		window.SITE_FIRST_HIT_AT = $('#js-settings').attr('data-first-hit-at') * 1000
 		window.GOATCOUNTER_COM   = $('#js-settings').attr('data-goatcounter-com') === 'true'
+		window.DEV               = $('#js-settings').attr('data-dev') === 'true'
 		window.USE_WEBSOCKET     = $('#js-settings').attr('data-websocket') === 'true'
 		window.WEBSOCKET         = undefined
 		if (!USER_SETTINGS.language)
@@ -21,6 +22,8 @@
 
 	// Set up error reporting.
 	let report_errors = function() {
+		if (window.DEV)
+			return
 		window.onerror = on_error
 		$(document).on('ajaxError', function(e, xhr, settings, err) {
 			if (settings.url === BASE_PATH + '/jserr')  // Just in case, otherwise we'll be stuck.

tpl/_backend_bottom.gohtml
@@ -9,6 +9,7 @@
 		data-first-hit-at="{{.Site.FirstHitAt.Unix}}"
 		data-websocket="{{.Websocket}}"
 		data-goatcounter-com="{{.GoatcounterCom}}"
+		data-dev="{{.Dev}}"
 		{{if .Base}}data-base-path="{{.Base}}"{{end}}
 		{{if .User.ID}}data-csrf="{{.User.CSRFToken}}"{{end}}
 	>

widgets/pages.go
@@ -121,6 +121,26 @@ func (w Pages) RenderHTML(ctx context.Context, shared SharedData) (string, any) 
 	}
 	t += ".gohtml"
 
+	// Set days in the future to -1; we filter this in the JS when rendering the
+	// chart. It's easier to do this here because JavaScript Date() has
+	// piss-poor support for timezones.
+	//
+	// Only remove them if the last day is today: for everything else we want to
+	// display the future as "greyed out".
+	if w.Style != "text" && len(w.Pages) > 0 && len(w.Pages[0].Stats) > 0 {
+		var (
+			now   = ztime.Now(ctx).In(goatcounter.MustGetUser(ctx).Settings.Timezone.Loc())
+			today = now.Format("2006-01-02")
+			hour  = now.Hour()
+		)
+		if w.Pages[0].Stats[len(w.Pages[0].Stats)-1].Day == today {
+			for i := range w.Pages {
+				j := len(w.Pages[i].Stats) - 1
+				w.Pages[i].Stats[j].Hourly = w.Pages[i].Stats[j].Hourly[:hour+1]
+			}
+		}
+	}
+
 	return t, struct {
 		Context context.Context
 		Site    *goatcounter.Site

widgets/total_pages.go
@@ -6,6 +6,7 @@ import (
 
 	"zgo.at/goatcounter/v2"
 	"zgo.at/z18n"
+	"zgo.at/zstd/ztime"
 )
 
 type TotalPages struct {
@@ -53,6 +54,22 @@ func (w *TotalPages) GetData(ctx context.Context, a Args) (more bool, err error)
 }
 
 func (w TotalPages) RenderHTML(ctx context.Context, shared SharedData) (string, any) {
+	// Set days in the future to -1; we filter this in the JS when rendering the
+	// chart. It's easier to do this here because JavaScript Date() has
+	// piss-poor support for timezones.
+	//
+	// Only remove them if the last day is today: for everything else we want to
+	// display the future as "greyed out".
+	var (
+		now   = ztime.Now(ctx).In(goatcounter.MustGetUser(ctx).Settings.Timezone.Loc())
+		today = now.Format("2006-01-02")
+		hour  = now.Hour()
+	)
+	if len(w.Total.Stats) > 0 && w.Total.Stats[len(w.Total.Stats)-1].Day == today {
+		j := len(w.Total.Stats) - 1
+		w.Total.Stats[j].Hourly = w.Total.Stats[j].Hourly[:hour+1]
+	}
+
 	return "_dashboard_totals.gohtml", struct {
 		Context context.Context
 		Site    *goatcounter.Site