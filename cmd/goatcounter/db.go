9f2bfb85fce9c6216773978f41c00464d3e58465
Martin Tournoij <martin@arp242.net>
2026-02-24 23:09:14 +0000

    Disable JIT by default in the Docker container
    It's basically never faster in my testing, and often quite a bit slower.

--- cmd/goatcounter/db.go
+++ cmd/goatcounter/db.go
@@ -301,14 +301,6 @@ PostgreSQL notes:
 
         PGDATABASE=goatcounter PGHOST=/var/run goatcounter -db 'postgresql'
 
-    You may want to consider lowering the "seq_page_cost" parameter; the query
-    planner tends to prefer seq scans instead of index scans for some
-    operations with the default of 4, which is much slower. I found that 1.1 is
-    a fairly good setting, you can set it in your postgresql.conf file, or just
-    for one database with:
-
-        alter database goatcounter set seq_page_cost=1.1
-
 Converting from SQLite to PostgreSQL:
 
     You can use pgloader (https://pgloader.io) to convert from a SQLite to
